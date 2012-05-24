d3b83a329c367153ebd31308b481c7282f95fe78
Martin Stein <martin.stein@genode-labs.com>
2012-05-24 12:57:36 +0200

    Simple driver for the SP804 timer.

--- base/include/drivers/timer/sp804_base.h
+++ base/include/drivers/timer/sp804_base.h
@@ -20,9 +20,7 @@
 namespace Genode
 {
 	/**
-	 * Basic driver for the ARM SP804 timer
-	 *
-	 * Uses only timer module 0.
+	 * Basic driver for the ARM SP804 timer that uses only timer module 0
 	 */
 	template <unsigned long CLK>
 	class Sp804_base : public Mmio
