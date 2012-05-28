411838ba0c110e5a55a85f913a33295caefa10ee
Martin Stein <martin.stein@genode-labs.com>
2012-05-28 11:47:25 +0200

    Specs for components that are targeted by base-hw.

---
+++ base/mk/spec-pl011.mk
@@ -0,0 +1,8 @@
+#
+# \brief  Make configurations specific for the ARM PL011
+# \author Martin Stein
+# \date   2011-12-20
+#
+
+# Add repository relative include paths
+REP_INC_DIR += include/pl011
---
+++ base/mk/spec-pl390.mk
@@ -0,0 +1,8 @@
+#
+# \brief  Make configurations specific for the ARM PL390
+# \author Martin Stein
+# \date   2011-12-20
+#
+
+# Add repository relative include paths
+REP_INC_DIR += include/pl390
