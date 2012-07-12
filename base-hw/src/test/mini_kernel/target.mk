0d1fc19410fcebd85025f7d4eb88999ce82f1108
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2012-07-12 14:13:59 +0200

    Finalize trustzone test-kernel
    The mini_kernel uses DDR RAM only (relocated at 0x60000000), and the
    memory mapped uart3 registers, as well as the one for the PIC. It programs
    the PIC to receive uart3 irqs, and every time it receives an irq prints
    a message to uart3.

--- base-hw/src/test/mini_kernel/target.mk
+++ base-hw/src/test/mini_kernel/target.mk
@@ -1,7 +1,6 @@
-TARGET      = mini_kernel
-STARTUP_LIB =
-SRC_CC      = main.cc
-SRC_S       = crt0.s exc_vector.s
-CC_OPT      = -fno-exceptions -fno-rtti
-
-vpath crt0.s $(REP_DIR)/src/platform
+TARGET       = mini_kernel
+STARTUP_LIB  =
+SRC_CC       = main.cc
+SRC_S        = crt0.s exc_vector.s
+CC_OPT       = -fno-exceptions -fno-rtti
+LD_TEXT_ADDR = 0x60000000
