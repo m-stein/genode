3b2570aa9bcbcb59e4249c7399342ba367207017
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2012-07-12 14:56:33 +0200

    Fix world-switch and world-switch inline assembler entry code.
    We have to save the lr register in addition to r0-r12. Simplify the world switch
    assembler code a bit more (e.g. more macros).

--- base-hw/src/core/arm_v7a/monitor_vector.s
+++ base-hw/src/core/arm_v7a/monitor_vector.s
@@ -32,6 +32,13 @@
 .endm
 
 
+.macro _mm_toggle_ns_bit reg bit
+	mov   \reg, #\bit
+	mcr   p15, 0, \reg, c1, c1, 0
+	isb
+.endm
+
+
 /**
  * Save secure state on top of the stack.
  *
@@ -39,45 +46,40 @@
  * restore some on FIQ.
  */
 .macro _mm_save_secure_state
-	stmdb sp!, {r3, r4}           /* save cpsr and return-eip of svc         */
-	stmdb sp,  {sp, lr}^          /* save user-level return values           */
-	sub   sp,  sp, #8             /* adjust stack pointer                    */
+	stmdb sp!, {r3, r4}             /* save cpsr and return-eip of svc       */
+	stmdb sp,  {sp, lr}^            /* save user-level return values         */
+	sub   sp,  sp, #8               /* adjust stack pointer                  */
 .endm
 
 
 .macro _mm_restore_normal_state
-	ldr   r0, [sp, #16]           /* load vm_state addr. from stack          */
-	add   r0, r0, #13*4           /* jump over general purpose register      */
-	ldmia r0, {sp, lr}^           /* sp_usr and lr_usr                       */
-	add   r0, r0, #8              /* adjust offset in r0                     */
-	_mm_restore_bank 18           /* load irq banks                          */
-	cps   #17                     /* switch to fiq mode                      */
-	ldmia r0!, {r8 - r12, sp, lr} /* load fiq registers                      */
-	ldmia r0!, {r1}               /* load spsr_fiq                           */
+	ldr   r0, [sp, #16]             /* load vm_state addr. from stack        */
+	add   r0, r0, #13*4             /* jump over general purpose register    */
+	ldmia r0, {sp, lr}^             /* sp_usr and lr_usr                     */
+	add   r0, r0, #8                /* adjust offset in r0                   */
+	_mm_restore_bank 18             /* load irq banks                        */
+	cps   #17                       /* switch to fiq mode                    */
+	ldmia r0!, {r8 - r12, sp, lr}   /* load fiq registers                    */
+	ldmia r0!, {r1}                 /* load spsr_fiq                         */
 	msr   spsr, r1
-	_mm_restore_bank 23           /* load abort banks                        */
-	_mm_restore_bank 27           /* load undefined banks                    */
-	_mm_restore_bank 19           /* load supervisor banks                   */
-	cps   #22                     /* switch back to monitor mode             */
-	ldmia r0!, {r1, r2}           /* copy return pc/cpsr on stack            */
+	_mm_restore_bank 23             /* load abort banks                      */
+	_mm_restore_bank 27             /* load undefined banks                  */
+	_mm_restore_bank 19             /* load supervisor banks                 */
+	cps   #22                       /* switch back to monitor mode           */
+	ldmia r0!, {r1, r2}             /* copy return pc/cpsr on stack          */
 	stmdb sp,  {r1, r2}
-	mov   r1, #1                  /* switch to non-secure state              */
-	mcr   p15, 0, r1, c1, c1, 0
-	isb                           /* instruction barrier                     */
-	ldmia r0!, {r1}               /* load floating-point excp. register      */
-	mcr   p10, 7, r1, cr8, cr0, 0
-	mov   r1, #0                  /* switch to secure state                  */
-	mcr   p15, 0, r1, c1, c1, 0
-	isb                           /* instruction barrier                     */
-	ldr   lr, [sp, #16]           /* load general-purpose register           */
+	_mm_toggle_ns_bit r1, 1
+	ldmia r0!, {r1}                 /* load floating-point excp. register    */
+#	mcr   p10, 7, r1, cr8, cr0, 0
+	_mm_toggle_ns_bit r1, 0
+	ldr   lr, [sp, #16]             /* load general-purpose register         */
 	ldmia lr!, {r0 - r12}
 .endm
 
 
 .macro _mm_save_normal_state
 	str   lr, [sp,#-12]             /* save exit reason temporarily on stack */
-	mov   lr, #0                    /* switch to secure state                */
-	mcr   p15, 0, lr, c1, c1, 0
+	_mm_toggle_ns_bit lr, 0
 	ldr   lr, [sp, #16]             /* save general-purpose registers        */
 	stmia lr!, {r0 - r12}
 	mov   r0, lr
@@ -95,14 +97,10 @@
 	sub   lr, sp, #8                /* copy return pc/cpsr from stack        */
 	ldmia lr, {r1, r2}
 	stmia r0!, {r1, r2}
-	mov   r1, #1                    /* switch to non-secure state            */
-	mcr   p15, 0, r1, c1, c1, 0
-	isb
-	mrc   p10, 7, r1, cr8, cr0, 0   /* store floating-point exc. register    */
+	_mm_toggle_ns_bit r1, 1
+#	mrc   p10, 7, r1, cr8, cr0, 0   /* store floating-point exc. register    */
 	stmia r0!, {r1}
-	mov   r1, #0                    /* switch to secure state                */
-	mcr   p15, 0, r1, c1, c1, 0
-	isb
+	_mm_toggle_ns_bit r1, 0
 	ldr   r1, [sp, #-12]            /* copy the exit reason from stack       */
 	stmia r0!,{r1}
 .endm
@@ -120,13 +118,13 @@
 
 
 .macro _mm_switch_to_secure nr, off
-	sub   lr, lr, #\off         /* adjust return eip according to exc.       */
-	srsdb sp, #0x16             /* store return state on mon_sp              */
-	mov   lr, #\nr              /* store exc. number in lr                   */
+	sub   lr, lr, #\off             /* adjust return eip according to exc.   */
+	srsdb sp, #22                   /* store return state on mon_sp          */
+	mov   lr, #\nr                  /* store exc. number in lr               */
 	_mm_save_normal_state
 	_mm_restore_secure_state
 	mov   lr, r3
-	msr   spsr_cfsx, r4         /* write cfsx flags to spsr register         */
+	msr   spsr_cfsx, r4             /* write cfsx flags to spsr register     */
 	movs  pc, lr
 .endm
 
@@ -151,21 +149,17 @@ _mm_vector_base:
 _mm_switch_to_normal:
 	_mm_save_secure_state
 	_mm_restore_normal_state
-	mov   lr, #1                /* switch to non-secure mode                 */
-	mcr   p15, 0, lr, c1, c1, 0
-	isb                         /* instruction barrier                       */
+	mov lr, #127
+#	mov lr, #125
+	mcr   p15, 0, lr, c1, c1, 0 /* enable AW, FW, EA, FIQ, and NS bit        */
+#	_mm_toggle_ns_bit lr, 1     /* switch to non-secure mode                 */
 	ldr   lr, [sp, #-4]
 	msr   spsr, lr              /* set spsr_mon with unsecure spsr           */
 	ldr   lr, [sp, #-8]         /* set lr_mon with unsecure ip               */
-	movs  pc, lr
+	subs  pc, lr, #0
 
-/**
- * Start with exception numbers after the ones for the secure world's
- * exception mode, so we are compatible with pre-existing kernel
- * routines.
- */
-_mm_smc_entry:        _mm_switch_to_secure 8  0 /* start behind smc inst.    */
-_mm_pf_abort_entry:   _mm_switch_to_secure 9  4 /* replay instruction        */
-_mm_data_abort_entry: _mm_switch_to_secure 10 4 /* replay instruction        */
-_mm_irq_entry:        _mm_switch_to_secure 11 4 /* replay instruction        */
-_mm_fiq_entry:        _mm_switch_to_secure 12 4 /* replay instruction        */
+_mm_smc_entry:        _mm_switch_to_secure 2 0
+_mm_pf_abort_entry:   _mm_switch_to_secure 3 4
+_mm_data_abort_entry: _mm_switch_to_secure 4 4
+_mm_irq_entry:        _mm_switch_to_secure 6 4
+_mm_fiq_entry:        _mm_switch_to_secure 7 4
--- base-hw/src/core/include/arm_v7a/trustzone.h
+++ base-hw/src/core/include/arm_v7a/trustzone.h
@@ -21,22 +21,23 @@
  */
 static inline void tz_switch_to_normal_world(Genode::Vm_state *state)
 {
-	extern char _mm_switch_to_normal;
+	extern void* _mm_switch_to_normal;
 
-	asm volatile("stmdb sp!, {r0-r12}    \n" /* save registers           */
-				 "stmdb sp!, {%[state]}  \n" /* save vm_state pointer    */
-				 "mov    r2, sp          \n" /* copy sp_svc to sp_mon    */
-				 "cps    #22             \n" /* switch to monitor mode   */
-				 "mov    sp, r2          \n"
-				 "adr    r3, 1f          \n" /* save return eip          */
-				 "mrs    r4, cpsr        \n" /* save return psr          */
-				 "mov    pc, %[instr]    \n" /* world switch routine     */
-				 "1:                     \n"
-				 "mov    r0, sp          \n" /* copy sp_mon to sp_svc    */
-				 "cps    #19             \n" /* switch to svc mode       */
-				 "mov    sp, r0          \n"
-				 "ldmia  sp!, {%[state]} \n" /* restore vm_state pointer */
-				 "ldmia  sp!, {r0-r12}   \n" /* restore registers        */
+	asm volatile("stmdb sp!, {r0-r12, lr}  \n" /* save registers           */
+				 "stmdb sp!, {%[state]}    \n" /* save vm_state pointer    */
+				 "mov    r0, %[instr]      \n" /* world switch routine     */
+				 "mov    r2, sp            \n" /* copy sp_svc to sp_mon    */
+				 "cps    #22               \n" /* switch to monitor mode   */
+				 "mov    sp, r2            \n"
+				 "adr    r3, 1f            \n" /* save return eip          */
+				 "mrs    r4, cpsr          \n" /* save return psr          */
+				 "mov    pc, r0            \n" /* world switch routine     */
+				 "1:                       \n"
+				 "mov    r0, sp            \n" /* copy sp_mon to sp_svc    */
+				 "cps    #19               \n" /* switch to svc mode       */
+				 "mov    sp, r0            \n"
+				 "ldmia  sp!, {%[state]}   \n" /* restore vm_state pointer */
+				 "ldmia  sp!, {r0-r12, lr} \n" /* restore registers        */
 				 : : [state] "r" (state), [instr] "r" (&_mm_switch_to_normal)
 				 : "memory");
 }
