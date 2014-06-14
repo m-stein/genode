cef756dbd4c9ded45cf91ffa91d71c957156abf6
Martin Stein <martin.stein@genode-labs.com>
2014-06-14 11:07:14 +0200

    base: specs platform-panda_a and platform-panda_b
    Support for the distinction of Panda Revison A and B at the level of the
    platform specifier. This is done to enable base-hw later to consider the
    correct board revision. However, at least base-foc does not need this
    information, which is why the specifier platform_panda is still available and
    references all aspects that are common to platform_panda_a and
    platform_panda_b.
    
    ref #1178

---
+++ repos/base/mk/spec-platform_panda_a.mk
@@ -0,0 +1,5 @@
+SPECS += platform_panda
+
+REP_INC_DIR += include/platform/panda_a
+
+include $(call select_from_repositories,mk/spec-platform_panda.mk)
---
+++ repos/base/mk/spec-platform_panda_b.mk
@@ -0,0 +1,5 @@
+SPECS += platform_panda
+
+REP_INC_DIR += include/platform/panda_b
+
+include $(call select_from_repositories,mk/spec-platform_panda.mk)
