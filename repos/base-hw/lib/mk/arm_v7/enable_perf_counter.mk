e6457defc6504e2b808fd56e41ebec0923559b63
Martin Stein <martin.stein@genode-labs.com>
2014-07-10 12:46:53 +0200

    hw: reorg sources in core/arm_v6 and core/arm_v7

--- repos/base-hw/lib/mk/arm_v7/enable_perf_counter.mk
+++ repos/base-hw/lib/mk/arm_v7/enable_perf_counter.mk
@@ -1,3 +1,11 @@
+#
+# \brief  Build config that enables performance counting
+# \author Josef Soentgen
+# \date   2013-09-26
+#
+
+# add C++ sources
 SRC_CC = perf_counter.cc
 
-vpath %.cc $(REP_DIR)/src/core/arm_v7
+# declare source paths
+vpath perf_counter.cc $(REP_DIR)/src/core/spec/arm_v7
