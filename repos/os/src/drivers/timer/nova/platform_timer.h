3a63f2486cfe2a0066a55a1e14ef7e59abbc8f82
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2014-07-22 12:07:49 +0200

    fixup: "timer: nova specific version of the service"

--- repos/os/src/drivers/timer/nova/platform_timer.h
+++ repos/os/src/drivers/timer/nova/platform_timer.h
@@ -25,7 +25,22 @@ class Platform_timer
 		unsigned long _timeout;
 		Genode::Trace::Timestamp _tsc_start;
 		unsigned long _tsc_khz;
-		
+
+		/* 1 / ((us / (1000 * 1000)) * (tsc_khz * 1000)) */
+		enum { TSC_FACTOR = 1000ULL };
+
+		/**
+		 * Convenience function to calculate time in us value out of tsc
+		 */
+		inline unsigned long _time_in_us(unsigned long long tsc,
+		                                 bool sub_tsc_start = true) const
+		{
+			if (sub_tsc_start)
+				return (tsc - _tsc_start) / (_tsc_khz / TSC_FACTOR);
+
+			return (tsc) / (_tsc_khz / TSC_FACTOR);
+		}
+ 
 	public:
 
 		/**
@@ -45,19 +60,16 @@ class Platform_timer
 
 		/**
 		 * Return current time-counter value in microseconds
-		 *
-		 * This function has to be executed regularly,
-		 * at least all max_timeout() usecs.
 		 */
 		unsigned long curr_time() const
 		{
-			return (Genode::Trace::timestamp() - _tsc_start) / (_tsc_khz / 1000);
+			return _time_in_us(Genode::Trace::timestamp());
 		}
 
 		/**
 		 * Return maximum timeout as supported by the platform
 		 */
-		unsigned long max_timeout() { return ~0UL; }
+		unsigned long max_timeout() { return _time_in_us(~0UL); }
 
 		/**
 		 * Schedule next timeout
@@ -75,6 +87,7 @@ class Platform_timer
 					nova_die();
 			}
 				
+			/* remember timeout to be set during wait_for_timeout call */
 			_timeout = timeout_usec;
 		}
 
@@ -86,23 +99,63 @@ class Platform_timer
 			using namespace Genode;
 			using namespace Nova;
 
+			/* XXX quirk start - description below */
+			static unsigned short quirk_count = 0;
+			Trace::Timestamp before = Trace::timestamp();
+			asm volatile ("":::"memory");
+			/* XXX quirk end */
+
 			if (_sem == ~0UL)
 				_sem = blocking_thread->tid().exc_pt_sel + SM_SEL_EC;
  
 			addr_t sem = _sem;
 
-			/* tsc_absolute == 0 means blocking without timeout */
-			unsigned long long tsc_absolute = 0;
 			/* calculate absolute timeout */
-			if (_timeout != max_timeout()) {
-				/* 1 / ((us / (1000 * 1000)) * (tsc_khz * 1000)) */
-				enum { FACTOR = 1000ULL };
+			Trace::Timestamp now   = Trace::timestamp();
+			Trace::Timestamp us_64 = _timeout; 
 
-				Trace::Timestamp us_64 = _timeout;
-				Trace::Timestamp now   = Trace::timestamp();
-				tsc_absolute           = now + us_64 * (_tsc_khz / FACTOR);
+			if (_timeout == max_timeout()) {
+				/* tsc_absolute == 0 means blocking without timeout */
+				Genode::uint8_t res = sm_ctrl(sem, SEMAPHORE_DOWN, 0);
+				if (res != Nova::NOVA_OK && res != Nova::NOVA_TIMEOUT)
+					nova_die();
+				return;
 			}
 
+			/*
+			 * XXX quirk start
+			 *
+			 * On some x86 platforms, it happens that the system seems to slow
+			 * down dramatically for some unclear reasons so far. When this
+			 * happens, the handling of the timeout queue and reprogramming the
+			 * next timeout takes so long that the timer IRQ will fire
+			 * immediately after acknowledging it. This causes the timer
+			 * service to run on a very high rate, which may utilize the CPU
+			 * close to the maximum. We try to detect this condition by the
+			 * following heuristic and apply this quirk, which programs the
+			 * next timeout later in time - so that it will fire not
+			 * immediately after acknowledging it.
+			 *
+			 * This quirk should be removed as soon as it is clear what
+			 * triggers the phenomenon.
+			 */
+			unsigned long diff = _time_in_us(now - before, false);
+
+			if (diff)
+				quirk_count++;
+			else
+				quirk_count = 0;
+
+			if (quirk_count > 10) {
+				us_64 += 30000;
+				PWRN("apply timer quirk - diff=%lu, delay timeout %lu->%llu us",
+				     diff, _timeout, us_64);
+				quirk_count = 0;
+			}
+			/* XXX quirk end */
+
+			/* block until timeout fires or it gets canceled */
+			unsigned long long tsc_absolute = now + us_64 * (_tsc_khz / TSC_FACTOR);
 			Genode::uint8_t res = sm_ctrl(sem, SEMAPHORE_DOWN, tsc_absolute);
 			if (res != Nova::NOVA_OK && res != Nova::NOVA_TIMEOUT)
 				nova_die();
