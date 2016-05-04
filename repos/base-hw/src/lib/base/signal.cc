79f18908ff119c7a705e949d6b917104e44ac770
Norman Feske <norman.feske@genode-labs.com>
2016-05-04 12:27:17 +0200

    Thread API cleanup
    This patch cleans up the thread API and comes with the following
    noteworthy changes:
    
    - Introduced Cpu_session::Weight type that replaces a formerly used
      plain integer value to prevent the accidental mix-up of
      arguments.
    - The enum definition of Cpu_session::DEFAULT_WEIGHT moved to
      Cpu_session::Weight::DEFAULT_WEIGHT
    - New Thread constructor that takes a 'Env &' as first argument.
      The original constructors are now marked as deprecated. For the
      common use case where the default 'Weight' and 'Affinity' are
      used, a shortcut is provided. In the long term, those two
      constructors should be the only ones to remain.
    - The former 'Thread<>' class template has been renamed to
      'Thread_deprecated'.
    - The former 'Thread_base' class is now called 'Thread'.
    - The new 'name()' accessor returns the thread's name as 'Name'
      object as centrally defined via 'Cpu_session::Name'. It is meant to
      replace the old-fashioned 'name' method that takes a buffer and size
      as arguments.
    - Adaptation of the thread test to the new API
    
    Issue #1954

--- repos/base-hw/src/lib/base/signal.cc
+++ repos/base-hw/src/lib/base/signal.cc
@@ -124,7 +124,7 @@ void Signal_receiver::block_for_signal()
 		return;
 	}
 	/* read signal data */
-	const void * const     utcb    = Thread_base::myself()->utcb()->data();
+	const void * const     utcb    = Thread::myself()->utcb()->data();
 	Signal::Data * const   data    = (Signal::Data *)utcb;
 	Signal_context * const context = data->context;
 	{
