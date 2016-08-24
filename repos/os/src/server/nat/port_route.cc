7192a308e2c95283d6fedd27c421bb8f34b095f6
Martin Stein <martin.stein@genode-labs.com>
2016-08-24 17:28:28 +0200

    nat: clean up port_route.*

--- repos/os/src/server/nat/port_route.cc
+++ repos/os/src/server/nat/port_route.cc
@@ -18,33 +18,37 @@ using namespace Net;
 using namespace Genode;
 
 
-Port_route::Port_route(uint16_t nr, char const * label, size_t label_size,
+Port_route::Port_route(uint16_t nr, char const *label, size_t label_size,
                        Ipv4_address via, Ipv4_address to)
 :
 	_nr(nr), _label(label, label_size), _via(via), _to(to)
 { }
 
 
-void Port_route::print(Output & output) const
+void Port_route::print(Output &output) const
 {
 	Genode::print(output, "", _nr, " -> \"", _label, "\" to ", _to,
 	              " via ", _via);
 }
 
 
-Port_route * Port_route::find_by_nr(uint16_t nr)
+Port_route *Port_route::find_by_nr(uint16_t nr)
 {
-	if (nr == _nr) { return this; }
-	bool side = nr > _nr;
+	if (nr == _nr) {
+		return this; }
+
+	bool const side = nr > _nr;
 	Port_route *c = Avl_node<Port_route>::child(side);
 	return c ? c->find_by_nr(nr) : 0;
 }
 
 
-Port_route * Port_route_tree::find_by_nr(uint16_t nr)
+Port_route *Port_route_tree::find_by_nr(uint16_t nr)
 {
-	Port_route * port = first();
-	if (!port) { return port; }
+	Port_route *port = first();
+	if (!port) {
+		return port; }
+
 	port = port->find_by_nr(nr);
 	return port;
 }
--- repos/os/src/server/nat/port_route.h
+++ repos/os/src/server/nat/port_route.h
@@ -29,6 +29,7 @@ namespace Net {
 	using Port_route_list = Genode::List<Port_route>;
 }
 
+
 class Net::Port_route : public Genode::Avl_node<Port_route>,
                         public Port_route_list::Element
 {
@@ -41,25 +42,36 @@ class Net::Port_route : public Genode::Avl_node<Port_route>,
 
 	public:
 
-		Port_route(Genode::uint16_t nr, char const * label,
+		Port_route(Genode::uint16_t nr, char const *label,
 		           Genode::size_t label_size, Ipv4_address via,
 		           Ipv4_address to);
 
-		void print(Genode::Output & output) const;
+		void print(Genode::Output &output) const;
 
-		Port_route * find_by_nr(Genode::uint16_t nr);
+		Port_route *find_by_nr(Genode::uint16_t nr);
 
-		bool higher(Port_route * p) { return p->_nr > _nr; }
 
-		Interface_label & label() { return _label; }
-		Ipv4_address via() { return _via; }
-		Ipv4_address to() { return _to; }
-		Genode::uint16_t nr() { return _nr; }
+		/**************
+		 ** Avl_node **
+		 **************/
+
+		bool higher(Port_route *route) { return route->_nr > _nr; }
+
+
+		/***************
+		 ** Accessors **
+		 ***************/
+
+		Interface_label &label() { return _label; }
+		Ipv4_address     via()   { return _via; }
+		Ipv4_address     to()    { return _to; }
+		Genode::uint16_t nr()    { return _nr; }
 };
+
 
 struct Net::Port_route_tree : Genode::Avl_tree<Port_route>
 {
-	Port_route * find_by_nr(Genode::uint16_t nr);
+	Port_route *find_by_nr(Genode::uint16_t nr);
 };
 
 #endif /* _PORT_ROUTE_H_ */
