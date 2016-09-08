51942001374de75b42902ddbd699fbaccbcc2475
Martin Stein <martin.stein@genode-labs.com>
2016-09-08 17:05:07 +0200

    fixup "timer/spec/hw: fix bug in curr_time"
    Ref #2088

--- repos/os/src/drivers/timer/spec/hw/platform_timer.h
+++ repos/os/src/drivers/timer/spec/hw/platform_timer.h
@@ -35,7 +35,7 @@ class Platform_timer
 		Kernel::capid_t const   _sigid;
 		unsigned long mutable   _curr_time_us;
 		Genode::Lock  mutable   _curr_time_us_lock;
-		unsigned long mutable   _consumed_us;
+		unsigned long mutable   _last_timeout_age_us;
 		time_t const            _max_timeout_us;
 
 		/**
@@ -55,7 +55,7 @@ class Platform_timer
 		Platform_timer()
 		:
 			_sigid(_capid(_sigrec.manage(&_sigctx))),
-			_curr_time_us(0), _consumed_us(0),
+			_curr_time_us(0), _last_timeout_age_us(0),
 			_max_timeout_us(Kernel::timeout_max_us())
 		{
 			Genode::log("maximum timeout ", _max_timeout_us, " us");
@@ -68,7 +68,7 @@ class Platform_timer
 		~Platform_timer() { _sigrec.dissolve(&_sigctx); }
 
 		/**
-		 * Refresh and return our instance-own "now"-time in microseconds
+		 * Refresh and return time in microseconds
 		 *
 		 * This function has to be executed regulary, at least all
 		 * max_timeout() us.
@@ -76,10 +76,12 @@ class Platform_timer
 		unsigned long curr_time() const
 		{
 			Genode::Lock::Guard lock(_curr_time_us_lock);
-			time_t const tout_age_us = Kernel::timeout_age_us();
-			if (tout_age_us > _consumed_us) {
-				_curr_time_us += tout_age_us - _consumed_us;
-				_consumed_us = tout_age_us;
+			time_t const timeout_age_us = Kernel::timeout_age_us();
+			if (timeout_age_us > _last_timeout_age_us) {
+
+				/* increment time by the difference since the last update */
+				_curr_time_us += timeout_age_us - _last_timeout_age_us;
+				_last_timeout_age_us = timeout_age_us;
 			}
 			return _curr_time_us;
 		}
@@ -110,7 +112,7 @@ class Platform_timer
 			 * timeout counter through 'curr_time()' (We rely on the fact that
 			 * this is done at least one time in every max-timeout period)
 			 */
-			_consumed_us = 0;
+			_last_timeout_age_us = 0;
 			Kernel::timeout(timeout_us, _sigid);
 		}
 
