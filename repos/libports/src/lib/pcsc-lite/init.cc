05eb14c29965f38b6277c12a86499bb094b2fd7e
Christian Prochaska <christian.prochaska@genode-labs.com>
2016-11-02 17:58:15 +0100

    fixup: 'libports: add pcsc-lite library' (initialization)
    Issue #2145

--- repos/libports/src/lib/pcsc-lite/init.cc
+++ repos/libports/src/lib/pcsc-lite/init.cc
@@ -29,51 +29,60 @@ extern "C" {
 
 static constexpr bool verbose = false;
 
-__attribute__((constructor)) void init_pcsc_lite()
+struct Pcsc_lite_initializer
 {
-	if (verbose) {
-		/* pcscd -f */
-		DebugLogSetLogType(DEBUGLOG_STDOUT_DEBUG);
-		/* pcscd -d */
-		DebugLogSetLevel(PCSC_LOG_DEBUG);
-		/* pcscd -a */
-		(void)DebugLogSetCategory(DEBUG_CATEGORY_APDU);
+	Pcsc_lite_initializer()
+	{
+		if (verbose) {
+			/* pcscd -f */
+			DebugLogSetLogType(DEBUGLOG_STDOUT_DEBUG);
+			/* pcscd -d */
+			DebugLogSetLevel(PCSC_LOG_DEBUG);
+			/* pcscd -a */
+			(void)DebugLogSetCategory(DEBUG_CATEGORY_APDU);
+		}
+
+		unsigned int vendor_id = 0x0000;
+		unsigned int product_id = 0x0000;
+
+		int fd = open("/config.pcsc-lite", O_RDONLY);
+
+		if (fd < 0) {
+			Genode::error("Could not open 'config.pcsc-lite'");
+			exit(1);
+		}
+		
+		char config[128];
+		if (read(fd, config, sizeof(config)) < 0) {
+			Genode::error("Could not read 'config.pcsc-lite'");
+			exit(1);
+		}
+
+		try {
+
+			Genode::Xml_node config_node(config);
+
+			vendor_id = config_node.attribute_value("vendor_id", 0U);
+			product_id = config_node.attribute_value("product_id", 0U);
+
+		} catch (...) {
+			Genode::error("Error parsing 'config.pcsc-lite'");
+			exit(1);
+		}
+
+		close(fd);
+
+		char device[14];
+
+		snprintf(device, sizeof(device), "usb:%04x/%04x", vendor_id, product_id);
+
+		RFAllocateReaderSpace(0);
+		(void)RFAddReader("CCID", 0, "/", device);
 	}
+};
 
-	unsigned int vendor_id = 0x0000;
-	unsigned int product_id = 0x0000;
 
-	int fd = open("/config.pcsc-lite", O_RDONLY);
-
-	if (fd < 0) {
-		Genode::error("Could not open 'config.pcsc-lite'");
-		exit(1);
-	}
-	
-	char config[128];
-	if (read(fd, config, sizeof(config)) < 0) {
-		Genode::error("Could not read 'config.pcsc-lite'");
-		exit(1);
-	}
-
-	try {
-
-		Genode::Xml_node config_node(config);
-
-		vendor_id = config_node.attribute_value("vendor_id", 0U);
-		product_id = config_node.attribute_value("product_id", 0U);
-
-	} catch (...) {
-		Genode::error("Error parsing 'config.pcsc-lite'");
-		exit(1);
-	}
-
-	close(fd);
-
-	char device[14];
-
-	snprintf(device, sizeof(device), "usb:%04x/%04x", vendor_id, product_id);
-
-	RFAllocateReaderSpace(0);
-	(void)RFAddReader("CCID", 0, "/", device);
+extern "C" void initialize_pcsc_lite()
+{
+	static Pcsc_lite_initializer pcsc_lite_initializer;
 }
