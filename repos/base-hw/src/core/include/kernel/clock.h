f5cee6b8b8673b7733c4e900cc9cbee1eabeec9a
Martin Stein <martin.stein@genode-labs.com>
2017-03-17 14:10:56 +0100

    hw: time syscall

--- repos/base-hw/src/core/include/kernel/clock.h
+++ repos/base-hw/src/core/include/kernel/clock.h
@@ -102,6 +102,8 @@ class Kernel::Clock
 		time_t timeout_age_us(Timeout const * const timeout) const;
 
 		time_t timeout_max_us() const;
+
+		time_t time() const { return _time; }
 };
 
 #endif /* _CORE__INCLUDE__KERNEL__CLOCK_H_ */
