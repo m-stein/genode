eec35f4cd6984ae74e1d3378a430713e4b51f62e
Martin Stein <martin.stein@genode-labs.com>
2018-01-02 19:23:32 +0100

    trace_logger: tmp

---
+++ repos/os/src/test/trace_subject_logger/main.cc
@@ -0,0 +1,19 @@
+/*
+ * \brief  Log information about present trace subjects
+ * \author Norman Feske
+ * \date   2015-06-15
+ */
+
+/*
+ * Copyright (C) 2015-2017 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+/* Genode includes */
+#include <base/component.h>
+
+using namespace Genode;
+
+void Component::construct(Genode::Env &env) { }
