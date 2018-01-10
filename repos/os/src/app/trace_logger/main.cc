f98c558d1220d75165959cd0a7a3eea94a381d79
Martin Stein <martin.stein@genode-labs.com>
2018-01-10 20:22:47 +0100

    tmp

--- repos/os/src/app/trace_logger/main.cc
+++ repos/os/src/app/trace_logger/main.cc
@@ -22,16 +22,19 @@
 using namespace Genode;
 
 
-class Buffer_monitor : public List<Buffer_monitor>::Element
+class Monitor : public List<Monitor>::Element
 {
 	private:
 
 		enum { MAX_ENTRY_BUF = 256 };
 
-		char                  _buf[MAX_ENTRY_BUF];
-		Region_map           &_rm;
-		Trace::Subject_id     _id;
-		Trace::Buffer        &_buffer;
+		Trace::Subject_id const  _id;
+		char                     _buf[MAX_ENTRY_BUF];
+		Region_map              &_rm;
+		Trace::Buffer           &_buffer;
+		unsigned long            _report_id { 0 };
+		Trace::Subject_info      _info                  { };
+		unsigned long long       _recent_exec_time { 0 };
 
 		const char *_terminate_entry(Trace::Buffer::Entry const &entry)
 		{
@@ -43,25 +46,31 @@ class Buffer_monitor : public List<Buffer_monitor>::Element
 
 	public:
 
-		Buffer_monitor(Region_map           &rm,
-		               Trace::Subject_id     id,
-		               Dataspace_capability  ds_cap)
+		Monitor(Trace::Subject_id     id,
+		        Region_map           &rm,
+		        Dataspace_capability  buffer_ds)
 		:
-			_rm(rm), _id(id),
-			_buffer(*(Trace::Buffer *)rm.attach(ds_cap))
+			_id(id), _rm(rm),
+			_buffer(*(Trace::Buffer *)rm.attach(buffer_ds))
 		{
-			log("monitor "
-			    "subject:", _id.id, " "
-			    "buffer:",  &_buffer);
+			log("new monitor: subject ", _id.id, " buffer ", &_buffer);
 		}
 
-		~Buffer_monitor()
+		~Monitor()
 		{
 			_buffer.~Buffer();
 			_rm.detach(&_buffer);
 		}
 
-		Trace::Subject_id id() { return _id; };
+		void update(Trace::Subject_info const &new_info)
+		{
+			unsigned long long const last_execution_time =
+				_info.execution_time().value;
+
+			_info = new_info;
+			_recent_exec_time =
+				_info.execution_time().value - last_execution_time;
+		}
 
 		void dump()
 		{
@@ -89,6 +98,18 @@ log("entry ", xxx++);
 			/* reset after we read all available entries */
 			_curr_entry = _buffer.first();
 		}
+
+
+		/***************
+		 ** Accessors **
+		 ***************/
+
+		Trace::Subject_id          id()               const { return _id; }
+		unsigned long long         recent_exec_time() const { return _recent_exec_time; }
+		unsigned long              report_id()        const { return _report_id; }
+		Trace::Subject_info const &info()             const { return _info; }
+
+		void incr_report_id() { _report_id++; }
 };
 
 
@@ -99,54 +120,41 @@ class Main
 		enum { MAX_SUBJECTS      = 512 };
 		enum { DEFAULT_PERIOD_MS = 5000 };
 
-		struct Entry : List<Entry>::Element
+		struct No_matching_monitor : Exception { };
+
+		Env                           &_env;
+		Trace::Connection              _trace            { _env, 1024*1024*10, 64*1024, 0 };
+		bool                           _report_affinity  { false };
+		bool                           _report_activity  { false };
+		Attached_rom_dataspace         _config_rom       { _env, "config" };
+		Xml_node                       _config           { _config_rom.xml() };
+		Timer::Connection              _timer            { _env };
+		Heap                           _heap             { _env.ram(), _env.rm() };
+		List<Monitor>                  _monitors         { };
+		String<32>                     _policy_name      { "rpc_name" };
+		Rom_connection                 _policy_rom       { _env, _policy_name.string() };
+		Rom_dataspace_capability       _policy_ds        { _policy_rom.dataspace() };
+		Session_label                  _policy_label     { "init -> test-trace_logger" };
+		size_t                         _policy_size      { Dataspace_client(_policy_ds).size() };
+		Trace::Policy_id               _policy_id        { _trace.alloc_policy(_policy_size) };
+		unsigned long                  _report_id        { 0 };
+		Trace::Subject_id              _subjects[MAX_SUBJECTS];
+		Trace::Subject_id              _traced_subjects[MAX_SUBJECTS];
+		Signal_handler<Main>           _config_handler   { _env.ep(), *this, &Main::_handle_config};
+		Microseconds                   _period_us        { _config.attribute_value("period_ms", (unsigned long)DEFAULT_PERIOD_MS) * 1000UL };
+		Timer::Periodic_timeout<Main>  _period           { _timer, *this, &Main::_handle_period, _period_us };
+		unsigned                       _num_traced       { 0 };
+
+		Monitor &_lookup_monitor(Trace::Subject_id const id)
 		{
-			Trace::Subject_id const id;
-			Trace::Subject_info     info                  { };
-			unsigned long long      recent_execution_time { 0 };
-
-			Entry(Trace::Subject_id id) : id(id) { }
-
-			void update(Trace::Subject_info const &new_info)
+			for (Monitor *monitor = _monitors.first(); monitor;
+			     monitor = monitor->List<Monitor>::Element::next())
 			{
-				unsigned long long const last_execution_time =
-					info.execution_time().value;
-
-				info = new_info;
-				recent_execution_time =
-					info.execution_time().value - last_execution_time;
+				if (monitor->id() == id)
+					return *monitor;
 			}
-		};
 
-		Env                      &_env;
-		Trace::Connection         _trace            { _env, 1024*1024*10, 64*1024, 0 };
-		unsigned long             _period_ms        { DEFAULT_PERIOD_MS };
-		bool                      _report_affinity  { false };
-		bool                      _report_activity  { false };
-		Attached_rom_dataspace    _config_rom       { _env, "config" };
-		Xml_node                  _config           { _config_rom.xml() };
-		Timer::Connection         _timer            { _env };
-		Heap                      _heap             { _env.ram(), _env.rm() };
-		List<Entry>               _entries          { };
-		List<Buffer_monitor>      _monitors         { };
-		String<32>                _policy_name      { "rpc_name" };
-		Rom_connection            _policy_rom       { _env, _policy_name.string() };
-		Rom_dataspace_capability  _policy_ds        { _policy_rom.dataspace() };
-		Session_label             _policy_label     { "init -> test-trace_logger" };
-		size_t                    _policy_size      { Dataspace_client(_policy_ds).size() };
-		Trace::Policy_id          _policy_id        { _trace.alloc_policy(_policy_size) };
-		unsigned long             _report_cnt       { 0 };
-		Trace::Subject_id         _subjects[MAX_SUBJECTS];
-		Trace::Subject_id         _traced_subjects[MAX_SUBJECTS];
-		Signal_handler<Main>      _config_handler   { _env.ep(), *this, &Main::_handle_config};
-		Signal_handler<Main>      _periodic_handler { _env.ep(), *this, &Main::_handle_period};
-
-		Entry *_lookup_entry(Trace::Subject_id const id)
-		{
-			for (Entry *e = _entries.first(); e; e = e->next())
-				if (e->id == id)
-					return e;
-			return nullptr;
+			throw No_matching_monitor();
 		}
 
 		bool _config_report_attribute_enabled(char const *attr) const
@@ -156,155 +164,120 @@ class Main
 			} catch (...) { return false; }
 		}
 
-		void _sort_by_recent_execution_time()
-		{
-//			List<Entry> sorted;
-//
-//			while (_entries.first()) {
-//
-//				/* find entry with lowest recent execution time */
-//				Entry *lowest = _entries.first();
-//				for (Entry *e = _entries.first(); e; e = e->next()) {
-//					if (e->recent_execution_time < lowest->recent_execution_time)
-//						lowest = e;
-//				}
-//
-//				_entries.remove(lowest);
-//				sorted.insert(lowest);
-//			}
-//
-//			_entries = sorted;
-		}
-
 		void _handle_config()
 		{
+			error(__func__, " not implemented");
 			_config_rom.update();
-
-			_period_ms = _config.attribute_value("period_ms", (unsigned long)DEFAULT_PERIOD_MS);
-
-			_report_affinity = _config_report_attribute_enabled("affinity");
-			_report_activity = _config_report_attribute_enabled("activity");
-
-			log("period_ms=",       _period_ms,       ", "
-			    "report_activity=", _report_activity, ", "
-			    "report_affinity=", _report_affinity);
-
-			_timer.trigger_periodic(1000*_period_ms);
 		}
 
-		void _handle_period()
+		void _handle_period(Duration)
 		{
-			_update_subjects(_trace, _heap);
-			_report_subjects(_report_affinity, _report_activity);
+			_update_subjects();
+			_report_subjects();
 		}
 
-		template <typename FUNC>
-		void _for_each_subject(FUNC const &func)
+		void _update_monitor(Monitor &monitor, Trace::Subject_id id)
 		{
-			for (Entry *entry = _entries.first(); entry; entry = entry->next()) {
-				Trace::Subject_info info = _trace.subject_info(entry->id);
-				func(entry->id, info);
+			monitor.update(_trace.subject_info(id));
+
+			bool label_match = false;
+			Session_label const label(monitor.info().session_label(),
+			                          " -> ",
+			                          monitor.info().thread_name().string());
+			try {
+				Session_policy policy(label, _config);
+				label_match = true;
+			}
+			catch (Session_policy::No_policy_defined) { }
+
+			/* purge dead threads */
+			if (monitor.info().state() == Trace::Subject_info::DEAD ||
+			    !label_match)
+			{
+				_trace.free(monitor.id());
+				_monitors.remove(&monitor);
+				destroy(_heap, &monitor);
+				_num_traced--;
 			}
 		}
 
-		void _update_subjects(Trace::Connection &trace, Allocator &alloc)
+		void _update_subjects()
 		{
-			unsigned const num_subjects = trace.subjects(_subjects, MAX_SUBJECTS);
-			unsigned       num_traced   = num_subjects;
+			unsigned const num_subjects = _trace.subjects(_subjects, MAX_SUBJECTS);
+			_num_traced = num_subjects;
 
-			/* add and update existing entries */
 			for (unsigned i = 0; i < num_subjects; i++) {
 
 				Trace::Subject_id const id = _subjects[i];
 
-				Entry *e = _lookup_entry(id);
-				if (!e) {
-					e = new (alloc) Entry(id);
-					_entries.insert(e);
-				}
+				try { _update_monitor(_lookup_monitor(id), id); }
+				catch (No_matching_monitor) {
 
-				e->update(trace.subject_info(id));
+					try {
+						_trace.trace(id.id, _policy_id, 16384U);
+						Dataspace_capability buffer_ds = _trace.buffer(id);
+						Monitor &monitor =
+							*new (_heap) Monitor(id, _env.rm(), buffer_ds);
 
-				bool label_match = false;
-				Session_label const label(e->info.session_label(),
-				                          " -> ",
-				                          e->info.thread_name().string());
-				try {
-					Session_policy policy(label, _config);
-					label_match = true;
-				}
-				catch (Session_policy::No_policy_defined) { }
+						_monitors.insert(&monitor);
+						_update_monitor(monitor, id);
 
-				/* purge dead threads */
-				if (e->info.state() == Trace::Subject_info::DEAD ||
-				    !label_match)
-				{
-					trace.free(e->id);
-					_entries.remove(e);
-					destroy(alloc, e);
-					log("do not trace \"", label, "\"");
-					num_traced--;
+					} catch (Trace::Source_is_dead) {
+
+						error("Failed to enable tracing");
+					}
 				}
 			}
-
-			log("trace ", num_traced, " out of ", num_subjects, " subjects");
-			_sort_by_recent_execution_time();
-
-
-			/* enable tracing for test-thread */
-			auto enable_tracing = [&] (Trace::Subject_id   id,
-			                           Trace::Subject_info info) {
-
-				while (Buffer_monitor *monitor = _monitors.first()) {
-					_monitors.remove(monitor);
-					destroy(alloc, monitor);
-				}
-
-				try {
-					log("enable tracing for "
-					    "thread:'", info.thread_name().string(), "' with "
-					    "policy:", _policy_id.id
-					);
-
-					trace.trace(id.id, _policy_id, 16384U);
-
-					Dataspace_capability ds_cap = trace.buffer(id.id);
-					_monitors.insert(new (alloc) Buffer_monitor(_env.rm(), id.id, ds_cap));
-
-				} catch (Trace::Source_is_dead) {
-					error("source is dead");
-					throw -2;
-				}
-			};
-
-			_for_each_subject(enable_tracing);
+			log("trace ", _num_traced, " out of ", num_subjects, " subjects");
 		}
 
-		void _report_subjects(bool report_affinity, bool report_activity)
+		void _report_subjects()
 		{
 			log("");
-			log("--- Report #", _report_cnt++, " ---");
-			for (Entry const *e = _entries.first(); e; e = e->next()) {
-
+			log("--- Report #", _report_id++, " ---");
+			for (Monitor const *monitor = _monitors.first(); monitor;
+			     monitor = monitor->next())
+			{
 				typedef Trace::Subject_info Subject_info;
-				Subject_info::State const state = e->info.state();
-				log("<subject label=\"",  e->info.session_label().string(),
-				          "\" thread=\"", e->info.thread_name().string(),
-				          "\" id=\"",     e->id.id,
+				Subject_info::State const state = monitor->info().state();
+				log("<subject label=\"",  monitor->info().session_label().string(),
+				          "\" thread=\"", monitor->info().thread_name().string(),
+				          "\" id=\"",     monitor->id().id,
 				          "\" state=\"",  Subject_info::state_name(state),
 				          "\">");
 
-				if (report_activity)
-					log("  <activity total=\"",  e->info.execution_time().value,
-					             "\" recent=\"", e->recent_execution_time,
+				if (_report_activity)
+					log("  <activity total=\"",  monitor->info().execution_time().value,
+					             "\" recent=\"", monitor->recent_exec_time(),
 					             "\">");
 
-				if (report_affinity)
-					log("  <affinity xpos=\"", e->info.affinity().xpos(),
-					             "\" ypos=\"", e->info.affinity().ypos(),
+				if (_report_affinity)
+					log("  <affinity xpos=\"", monitor->info().affinity().xpos(),
+					             "\" ypos=\"", monitor->info().affinity().ypos(),
 					             "\">");
 
 				log("</subject>");
+			}
+		}
+
+		void _install_policy()
+		{
+			try {
+				Dataspace_capability policy_dst_ds = _trace.policy(_policy_id);
+
+				if (policy_dst_ds.valid()) {
+					void *dst = _env.rm().attach(policy_dst_ds);
+					void *src = _env.rm().attach(_policy_ds);
+					memcpy(dst, src, _policy_size);
+					_env.rm().detach(dst);
+					_env.rm().detach(src);
+				}
+				log("load module: '", _policy_name, "' for "
+				    "label: '", _policy_label, "'");
+			} catch (...) {
+				error("could not load module '", _policy_name, "' for "
+				      "label '", _policy_label, "'");
+				throw;
 			}
 		}
 
@@ -314,29 +287,7 @@ class Main
 		{
 			_config_rom.sigh(_config_handler);
 			_handle_config();
-			_timer.sigh(_periodic_handler);
-
-			try {
-//				Rom_connection policy_rom(env, policy_module.string());
-//				policy_module_rom_ds = policy_rom.dataspace();
-//				size_t rom_size = Dataspace_client(policy_module_rom_ds).size();
-//				policy_id = trace.alloc_policy(rom_size);
-				Dataspace_capability policy_dst_ds = _trace.policy(_policy_id);
-
-				if (policy_dst_ds.valid()) {
-					void *dst = env.rm().attach(policy_dst_ds);
-					void *src = env.rm().attach(_policy_ds);
-					memcpy(dst, src, _policy_size);
-					env.rm().detach(dst);
-					env.rm().detach(src);
-				}
-				log("load module: '", _policy_name, "' for "
-				    "label: '", _policy_label, "'");
-			} catch (...) {
-				error("could not load module '", _policy_name, "' for "
-				      "label '", _policy_label, "'");
-				throw;
-			}
+			_install_policy();
 		}
 };
 
