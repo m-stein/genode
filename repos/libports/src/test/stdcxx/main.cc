98d6273271b99179af85323ea7a4b5b001491afe
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-01-22 14:40:04 +0100

    stdcxx: define _GLIBCXX11_USE_C99_STDLIB
    Fixes #2652

--- repos/libports/src/test/stdcxx/main.cc
+++ repos/libports/src/test/stdcxx/main.cc
@@ -48,6 +48,7 @@ static void test_cstdlib()
 {
 	static ::lldiv_t o __attribute__((used));
 	std::cout << std::strtoul("123", 0, 10) << std::endl;
+	std::cout << std::stoi("456") << std::endl;
 }
 
 
