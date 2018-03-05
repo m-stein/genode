7b23819796c6934bcc174193280fdcdeb98fdc41
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2018-03-05 20:32:41 +0100

    fixup "depot: recipe for Seoul VMM"

--- repos/ports/recipes/src/seoul/content.mk
+++ repos/ports/recipes/src/seoul/content.mk
@@ -2,13 +2,25 @@ PORT_DIR_SEOUL := $(call port_dir,$(REP_DIR)/ports/seoul)
 
 SRC_DIR = src/app/seoul
 
-content: $(SRC_DIR)
+content: $(SRC_DIR) src/include
 
 $(SRC_DIR):
 	mkdir -p $@
 	cp -rH $(REP_DIR)/$@/* $@/
 	cp -r $(PORT_DIR_SEOUL)/$@/* $@/
 	cp $(PORT_DIR_SEOUL)/$@/LICENSE .
+
+BASE_SRC_INCLUDE := src/include/base/internal/crt0.h \
+                    src/include/base/internal/globals.h \
+                    src/include/base/internal/unmanaged_singleton.h
+
+src/include:
+	mkdir -p $(dir $@)
+	mkdir -p $@/base/internal
+	cp -r $(GENODE_DIR)/repos/base-nova/$@ $(dir $@)
+	for file in $(BASE_SRC_INCLUDE); do \
+		cp $(GENODE_DIR)/repos/base/$$file $$file; \
+	done
 
 MIRROR_FROM_PORT_DIR := lib/mk/seoul_libc_support.mk \
                         include/vmm
@@ -32,7 +44,6 @@ MIRROR_FROM_BASE_NOVA := lib/mk/base-nova-common.mk \
                          lib/mk/base-nova.mk \
                          lib/mk/spec/x86_32/startup-nova.mk \
                          lib/mk/spec/x86_64/startup-nova.mk \
-                         src/include \
                          src/lib/base
 
 content: $(MIRROR_FROM_BASE_NOVA)
@@ -41,10 +52,7 @@ $(MIRROR_FROM_BASE_NOVA):
 	mkdir -p $(dir $@)
 	cp -r $(GENODE_DIR)/repos/base-nova/$@ $(dir $@)
 
-MIRROR_FROM_BASE := src/include/base/internal/crt0.h \
-                    src/include/base/internal/globals.h \
-                    src/include/base/internal/unmanaged_singleton.h \
-                    lib/mk/cxx.mk
+MIRROR_FROM_BASE := lib/mk/cxx.mk
 
 content: $(MIRROR_FROM_BASE)
 
