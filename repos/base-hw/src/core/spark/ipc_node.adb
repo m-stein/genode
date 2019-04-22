aa17e93d0a659efef1ccd27a3c2082ca2f09f070
Martin Stein <martin.stein@genode-labs.com>
2019-04-22 21:25:06 +0200

    spunky: further IPC_Node member variables

--- repos/base-hw/src/core/spark/ipc_node.adb
+++ repos/base-hw/src/core/spark/ipc_node.adb
@@ -5,10 +5,17 @@ package body IPC_Node is
    function Object_Size (Obj : Object_Type) return Object_Size_Type
    is (Obj'Size / 8);
 
-   procedure Initialize_Object (Obj : out Object_Type)
+   procedure Initialize_Object (
+      Obj  : out Object_Type;
+      Thrd :     Thread_Reference_Type)
    is
    begin
-      Obj := (Help => False);
+      Obj := (
+         Thread => Thrd,
+         State  => Inactive,
+         Caller => null,
+         Callee => null,
+         Help   => False);
    end Initialize_Object;
 
 end IPC_Node;
