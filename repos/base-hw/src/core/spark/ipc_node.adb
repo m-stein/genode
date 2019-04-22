5a6a37c7b38081bbae42e70374222c4b9e043ad0
Martin Stein <martin.stein@genode-labs.com>
2019-04-22 23:11:29 +0200

    spunky: queue members in IPC node

--- repos/base-hw/src/core/spark/ipc_node.adb
+++ repos/base-hw/src/core/spark/ipc_node.adb
@@ -2,20 +2,39 @@ pragma Ada_2012;
 
 package body IPC_Node is
 
-   function Object_Size (Obj : Object_Type) return Object_Size_Type
-   is (Obj'Size / 8);
+   package body Queue is
+
+      package body Item is
+
+         function Initialized_Object (Payload : Object_Reference_Type)
+         return Ibject_Type
+         is (
+            Next    => null,
+            Payload => Payload);
+
+      end Item;
+
+      function Initialized_Object return Qbject_Type
+      is (
+         Head => null,
+         Tail => null);
+
+   end Queue;
 
    procedure Initialize_Object (
-      Obj  : out Object_Type;
-      Thrd :     Thread_Reference_Type)
+      Obj  : Object_Reference_Type;
+      Thrd : Thread_Reference_Type)
    is
    begin
-      Obj := (
-         Thread => Thrd,
-         State  => Inactive,
-         Caller => null,
-         Callee => null,
-         Help   => False);
+      Obj.all := (
+         Thread             => Thrd,
+         State              => Inactive,
+         Caller             => null,
+         Callee             => null,
+         Help               => False,
+         Request_Queue      => Queue.Initialized_Object,
+         Request_Queue_Item => Queue.Item.Initialized_Object (Obj));
+
    end Initialize_Object;
 
 end IPC_Node;
--- repos/base-hw/src/core/spark/ipc_node.ads
+++ repos/base-hw/src/core/spark/ipc_node.ads
@@ -3,29 +3,60 @@ pragma Ada_2012;
 package IPC_Node is
 
    type Byte_Type             is range 0 .. 2**8 - 1 with Size => 8;
-   type Object_Size_Type      is range 0 .. 2**32 - 1 with Size => 32;
    type Thread_Type           is array (1 .. 32) of Byte_Type;
    type Thread_Reference_Type is not null access Thread_Type;
-   type Object_Type           is private;
-   type Object_Reference_Type is not null access Thread_Type;
+   type State_Type            is (Inactive, Wait_For_Reply, Wait_For_Request);
 
-   function Object_Size (Obj : Object_Type) return Object_Size_Type;
+   type Object_Type           is private;
+   type Object_Pointer_Type   is access Object_Type;
+   type Object_Reference_Type is not null access Object_Type;
+
+   package Queue is
+
+      package Item is
+
+         type Ibject_Type         is private;
+         type Ibject_Pointer_Type is access Ibject_Type;
+
+         function Initialized_Object (Payload : Object_Reference_Type)
+         return Ibject_Type;
+
+      private
+
+         type Ibject_Type is record
+            Next    : Ibject_Pointer_Type;
+            Payload : Object_Reference_Type;
+         end record;
+
+      end Item;
+
+      type Qbject_Type is private;
+
+      function Initialized_Object return Qbject_Type;
+
+   private
+
+      type Qbject_Type is record
+         Head : Item.Ibject_Pointer_Type;
+         Tail : Item.Ibject_Pointer_Type;
+      end record;
+
+   end Queue;
 
    procedure Initialize_Object (
-      Obj  : out Object_Type;
-      Thrd :     Thread_Reference_Type);
+      Obj  : Object_Reference_Type;
+      Thrd : Thread_Reference_Type);
 
 private
 
-   type State_Type          is (Inactive, Wait_For_Reply, Wait_For_Request);
-   type Object_Pointer_Type is access Object_Type;
-
    type Object_Type is record
-      Thread : Thread_Reference_Type;
-      State  : State_Type;
-      Caller : Object_Pointer_Type;
-      Callee : Object_Pointer_Type;
-      Help   : Boolean;
+      Thread             : Thread_Reference_Type;
+      State              : State_Type;
+      Caller             : Object_Pointer_Type;
+      Callee             : Object_Pointer_Type;
+      Help               : Boolean;
+      Request_Queue      : Queue.Qbject_Type;
+      Request_Queue_Item : Queue.Item.Ibject_Type;
    end record;
 
 end IPC_Node;
