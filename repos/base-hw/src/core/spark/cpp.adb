4cc36fec9ed934da8a9d98f96cabc6ca1ecc6992
Martin Stein <martin.stein@genode-labs.com>
2019-04-24 16:39:39 +0200

    spunky: IPC_Node.Can_Send_Request

---
+++ repos/base-hw/src/core/spark/cpp.adb
@@ -0,0 +1,25 @@
+--
+--  \brief  General types and subprograms for the glue between Ada and C++
+--  \author Martin stein
+--  \date   2019-04-24
+--
+
+--
+--  Copyright (C) 2019 Genode Labs GmbH
+--
+--  This file is part of the Genode OS framework, which is distributed
+--  under the terms of the GNU Affero General Public License version 3.
+--
+
+pragma Ada_2012;
+
+package body CPP is
+
+   function Bool_From_Ada (Value : Boolean)
+   return Bool_Type
+   is (
+      case Value is
+      when False => 0,
+      when True  => 1);
+
+end CPP;
--- repos/base-hw/src/core/spark/cpp_ipc_node.adb
+++ repos/base-hw/src/core/spark/cpp_ipc_node.adb
@@ -1,3 +1,16 @@
+--
+--  \brief  Glue code between Ada and C++ interface of IPC node
+--  \author Martin stein
+--  \date   2019-04-24
+--
+
+--
+--  Copyright (C) 2019 Genode Labs GmbH
+--
+--  This file is part of the Genode OS framework, which is distributed
+--  under the terms of the GNU Affero General Public License version 3.
+--
+
 pragma Ada_2012;
 
 package body CPP_IPC_Node is
@@ -9,6 +22,10 @@ package body CPP_IPC_Node is
    begin
       IPC_Node.Initialize_Object (Obj, Thrd);
    end Initialize_Object;
+
+   function Can_Send_Request (Obj : IPC_Node.Object_Reference_Type)
+   return CPP.Bool_Type
+   is (CPP.Bool_From_Ada (IPC_Node.Can_Send_Request (Obj)));
 
    function Thread (Obj : IPC_Node.Object_Reference_Type)
    return IPC_Node.Thread_Reference_Type
