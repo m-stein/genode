d757920eeabf50c2a594c33807915cf4a128f85d
Martin Stein <martin.stein@genode-labs.com>
2019-04-24 20:05:41 +0200

    spunky: clean up Ada sources

--- repos/base-hw/src/core/ada/cpp_thread.ads
+++ repos/base-hw/src/core/ada/cpp_thread.ads
@@ -13,10 +13,11 @@
 
 pragma Ada_2012;
 
+with CPP;
+
 package CPP_Thread is
 
-   type Byte_Type             is range 0 .. 2**8 - 1 with Size => 8;
-   type Object_Type           is array (1 .. 32) of Byte_Type;
+   type Object_Type           is private;
    type Object_Reference_Type is not null access Object_Type;
 
    procedure IPC_Wait_For_Request_Succeeded (Obj : Object_Reference_Type)
@@ -32,5 +33,9 @@ package CPP_Thread is
       Import,
       Convention    => C,
       External_Name => "_ZN6Kernel6Thread12ipc_copy_msgERS0_";
+
+private
+
+   type Object_Type is array (1 .. 32) of CPP.Byte_Type;
 
 end CPP_Thread;
