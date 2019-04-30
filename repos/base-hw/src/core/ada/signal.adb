60a7718bf08bdf52643e12839287526b571f7533
Martin Stein <martin.stein@genode-labs.com>
2019-04-30 15:09:57 +0200

    spunky: signal 4

--- repos/base-hw/src/core/ada/signal.adb
+++ repos/base-hw/src/core/ada/signal.adb
@@ -57,6 +57,47 @@ package body Signal is
    end Handler_Cancel_Waiting;
 
    --
+   --  Context_Killer_Initialize
+   --
+   procedure Context_Killer_Initialize (
+      Obj  : Context_Killer_Reference_Type;
+      Thrd : CPP_Thread.Object_Reference_Type)
+   is
+   begin
+      Obj.all := (
+         Thread  => Thrd,
+         Context => null);
+   end Context_Killer_Initialize;
+
+   --
+   --  Context_Killer_Deinitialize
+   --
+   procedure Context_Killer_Deinitialize (Obj : Context_Killer_Reference_Type)
+   is
+   begin
+      Context_Killer_Cancel_Waiting (Obj);
+   end Context_Killer_Deinitialize;
+
+   --
+   --  Context_Killer_Cancel_Waiting
+   --
+   procedure Context_Killer_Cancel_Waiting (
+      Obj : Context_Killer_Reference_Type)
+   is
+   begin
+      if Obj.Context = null then
+         return;
+      end if;
+      declare
+         Contxt : constant Context_Reference_Type :=
+            Context_Reference_Type (Obj.Context);
+      begin
+         Contxt.Killer := null;
+      end;
+      Obj.Context := null;
+   end Context_Killer_Cancel_Waiting;
+
+   --
    --  Context_Initialize
    --
    procedure Context_Initialize (
@@ -66,15 +107,27 @@ package body Signal is
    is
    begin
       Obj.all := (
-         Deliver_Queue_Item => Context_Queue.Initialized_Item_Object (Obj),
-         Context_Queue_Item => Context_Queue.Initialized_Item_Object (Obj),
-         Receiver           => Recvr,
-         Imprint            => Impr,
-         Killer             => null,
-         Nr_Of_Submits      => 0,
-         Acknowledged       => True,
-         Killed             => False);
+         Deliver_Item  => Context_Queue.Initialized_Item_Object (Obj),
+         Contexts_Item => Context_Queue.Initialized_Item_Object (Obj),
+         Receiver      => Recvr,
+         Imprint       => Impr,
+         Killer        => null,
+         Nr_Of_Submits => 0,
+         Acknowledged  => True,
+         Killed        => False);
    end Context_Initialize;
+
+   --
+   --  Receiver_Initialize
+   --
+   procedure Receiver_Initialize (Obj : Receiver_Reference_Type)
+   is
+   begin
+      Obj.all := (
+         Handlers => Handler_Queue.Initialized_Object,
+         Deliver  => Context_Queue.Initialized_Object,
+         Contexts => Context_Queue.Initialized_Object);
+   end Receiver_Initialize;
 
    --
    --  Handler_Queue
@@ -85,7 +138,7 @@ package body Signal is
       --  Initialized_Item_Object
       --
       function Initialized_Item_Object (Payload : Handler_Reference_Type)
-      return Ibject_Type
+      return Item_Type
       is (
          Next    => null,
          Payload => Payload);
@@ -93,7 +146,7 @@ package body Signal is
       --
       --  Initialized_Object
       --
-      function Initialized_Object return Qbject_Type
+      function Initialized_Object return Queue_Type
       is (
          Head => null,
          Tail => null);
@@ -102,10 +155,10 @@ package body Signal is
       --  Enqueue
       --
       procedure Enqueue (
-         Obj : in out Qbject_Type;
-         Itm :        Ibject_Reference_Type)
+         Obj : in out Queue_Type;
+         Itm :        Item_Reference_Type)
       is
-         Itm_Ptr : constant Ibject_Pointer_Type := Ibject_Pointer_Type (Itm);
+         Itm_Ptr : constant Item_Pointer_Type := Item_Pointer_Type (Itm);
       begin
          --  disconnect item from last queue
          Itm.Next := null;
@@ -126,10 +179,10 @@ package body Signal is
       --  Remove
       --
       procedure Remove (
-         Obj : in out Qbject_Type;
-         Itm : Ibject_Reference_Type)
+         Obj : in out Queue_Type;
+         Itm : Item_Reference_Type)
       is
-         Curr_Itm : Ibject_Pointer_Type := Obj.Head;
+         Curr_Itm : Item_Pointer_Type := Obj.Head;
       begin
          --  abort if queue is empty
          if Empty (Obj) then
@@ -137,7 +190,7 @@ package body Signal is
          end if;
 
          --  if specified item is the first of the queue
-         if Obj.Head = Ibject_Pointer_Type (Itm) then
+         if Obj.Head = Item_Pointer_Type (Itm) then
             Obj.Head := Itm.Next;
             if Obj.Head = null then
                Obj.Tail := null;
@@ -149,7 +202,7 @@ package body Signal is
             --  search for specified item in the queue
             Items_Loop :
             while Curr_Itm.Next /= null and
-                  Curr_Itm.Next /= Ibject_Pointer_Type (Itm)
+                  Curr_Itm.Next /= Item_Pointer_Type (Itm)
             loop
                Curr_Itm := Curr_Itm.Next;
             end loop Items_Loop;
@@ -174,9 +227,9 @@ package body Signal is
       --
       --  Dequeue
       --
-      procedure Dequeue (Obj : in out Qbject_Type)
+      procedure Dequeue (Obj : in out Queue_Type)
       is
-         Dequeued : constant Ibject_Pointer_Type := Obj.Head;
+         Dequeued : constant Item_Pointer_Type := Obj.Head;
       begin
          --  if there is none or only one item, empty the queue
          if Obj.Head = Obj.Tail then
@@ -197,7 +250,7 @@ package body Signal is
       --
       --  Empty
       --
-      function Empty (Obj : Qbject_Type)
+      function Empty (Obj : Queue_Type)
       return Boolean
       is (Obj.Tail = null);
 
@@ -205,16 +258,16 @@ package body Signal is
       --  Accessors  --
       -----------------
 
-      function Head (Obj : Qbject_Type)
-      return Ibject_Pointer_Type
+      function Head (Obj : Queue_Type)
+      return Item_Pointer_Type
       is (Obj.Head);
 
-      function Item_Payload (Itm : Ibject_Reference_Type)
+      function Item_Payload (Itm : Item_Reference_Type)
       return Handler_Reference_Type
       is (Itm.Payload);
 
-      function Item_Next (Itm : Ibject_Reference_Type)
-      return Ibject_Pointer_Type
+      function Item_Next (Itm : Item_Reference_Type)
+      return Item_Pointer_Type
       is (Itm.Next);
 
    end Handler_Queue;
@@ -228,7 +281,7 @@ package body Signal is
       --  Initialized_Item_Object
       --
       function Initialized_Item_Object (Payload : Context_Reference_Type)
-      return Ibject_Type
+      return Item_Type
       is (
          Next    => null,
          Payload => Payload);
@@ -236,7 +289,7 @@ package body Signal is
       --
       --  Initialized_Object
       --
-      function Initialized_Object return Qbject_Type
+      function Initialized_Object return Queue_Type
       is (
          Head => null,
          Tail => null);
@@ -245,10 +298,10 @@ package body Signal is
       --  Enqueue
       --
       procedure Enqueue (
-         Obj : in out Qbject_Type;
-         Itm :        Ibject_Reference_Type)
+         Obj : in out Queue_Type;
+         Itm :        Item_Reference_Type)
       is
-         Itm_Ptr : constant Ibject_Pointer_Type := Ibject_Pointer_Type (Itm);
+         Itm_Ptr : constant Item_Pointer_Type := Item_Pointer_Type (Itm);
       begin
          --  disconnect item from last queue
          Itm.Next := null;
@@ -269,10 +322,10 @@ package body Signal is
       --  Remove
       --
       procedure Remove (
-         Obj : in out Qbject_Type;
-         Itm : Ibject_Reference_Type)
+         Obj : in out Queue_Type;
+         Itm : Item_Reference_Type)
       is
-         Curr_Itm : Ibject_Pointer_Type := Obj.Head;
+         Curr_Itm : Item_Pointer_Type := Obj.Head;
       begin
          --  abort if queue is empty
          if Empty (Obj) then
@@ -280,7 +333,7 @@ package body Signal is
          end if;
 
          --  if specified item is the first of the queue
-         if Obj.Head = Ibject_Pointer_Type (Itm) then
+         if Obj.Head = Item_Pointer_Type (Itm) then
             Obj.Head := Itm.Next;
             if Obj.Head = null then
                Obj.Tail := null;
@@ -292,7 +345,7 @@ package body Signal is
             --  search for specified item in the queue
             Items_Loop :
             while Curr_Itm.Next /= null and
-                  Curr_Itm.Next /= Ibject_Pointer_Type (Itm)
+                  Curr_Itm.Next /= Item_Pointer_Type (Itm)
             loop
                Curr_Itm := Curr_Itm.Next;
             end loop Items_Loop;
@@ -317,9 +370,9 @@ package body Signal is
       --
       --  Dequeue
       --
-      procedure Dequeue (Obj : in out Qbject_Type)
+      procedure Dequeue (Obj : in out Queue_Type)
       is
-         Dequeued : constant Ibject_Pointer_Type := Obj.Head;
+         Dequeued : constant Item_Pointer_Type := Obj.Head;
       begin
          --  if there is none or only one item, empty the queue
          if Obj.Head = Obj.Tail then
@@ -340,7 +393,7 @@ package body Signal is
       --
       --  Empty
       --
-      function Empty (Obj : Qbject_Type)
+      function Empty (Obj : Queue_Type)
       return Boolean
       is (Obj.Tail = null);
 
@@ -348,16 +401,16 @@ package body Signal is
       --  Accessors  --
       -----------------
 
-      function Head (Obj : Qbject_Type)
-      return Ibject_Pointer_Type
+      function Head (Obj : Queue_Type)
+      return Item_Pointer_Type
       is (Obj.Head);
 
-      function Item_Payload (Itm : Ibject_Reference_Type)
+      function Item_Payload (Itm : Item_Reference_Type)
       return Context_Reference_Type
       is (Itm.Payload);
 
-      function Item_Next (Itm : Ibject_Reference_Type)
-      return Ibject_Pointer_Type
+      function Item_Next (Itm : Item_Reference_Type)
+      return Item_Pointer_Type
       is (Itm.Next);
 
    end Context_Queue;
--- repos/base-hw/src/core/ada/signal.ads
+++ repos/base-hw/src/core/ada/signal.ads
@@ -17,15 +17,17 @@ with CPP_Thread;
 
 package Signal is
 
-   type Imprint_Type            is mod 2**64;
-   type Number_Of_Submits_Type  is range 0 .. 2**32 - 1;
-   type Handler_Type            is private;
-   type Context_Type            is private;
-   type Context_Killer_Type     is private;
-   type Receiver_Type           is private;
-   type Handler_Reference_Type  is not null access all Handler_Type;
-   type Context_Reference_Type  is not null access all Context_Type;
-   type Receiver_Reference_Type is not null access all Receiver_Type;
+   type Imprint_Type                  is mod 2**64;
+   type Number_Of_Submits_Type        is range 0 .. 2**32 - 1;
+   type Handler_Type                  is private;
+   type Context_Type                  is private;
+   type Context_Killer_Type           is private;
+   type Receiver_Type                 is private;
+   type Handler_Reference_Type        is not null access all Handler_Type;
+   type Context_Reference_Type        is not null access all Context_Type;
+   type Receiver_Reference_Type       is not null access all Receiver_Type;
+   type Context_Killer_Reference_Type is not null access all
+                                            Context_Killer_Type;
 
    --
    --  Handler_Initialize
@@ -45,12 +47,35 @@ package Signal is
    procedure Handler_Cancel_Waiting (Obj : Handler_Reference_Type);
 
    --
+   --  Context_Killer_Initialize
+   --
+   procedure Context_Killer_Initialize (
+      Obj  : Context_Killer_Reference_Type;
+      Thrd : CPP_Thread.Object_Reference_Type);
+
+   --
+   --  Context_Killer_Deinitialize
+   --
+   procedure Context_Killer_Deinitialize (Obj : Context_Killer_Reference_Type);
+
+   --
+   --  Context_Killer_Cancel_Waiting
+   --
+   procedure Context_Killer_Cancel_Waiting (
+      Obj : Context_Killer_Reference_Type);
+
+   --
    --  Context_Initialize
    --
    procedure Context_Initialize (
       Obj   : Context_Reference_Type;
       Recvr : Receiver_Reference_Type;
       Impr  : Imprint_Type);
+
+   --
+   --  Receiver_Initialize
+   --
+   procedure Receiver_Initialize (Obj : Receiver_Reference_Type);
 
 private
 
@@ -59,47 +84,47 @@ private
    --
    package Handler_Queue is
 
-      type Qbject_Type           is private;
-      type Ibject_Type           is private;
-      type Ibject_Pointer_Type   is access all Ibject_Type;
-      type Ibject_Reference_Type is not null access all Ibject_Type;
+      type Queue_Type           is private;
+      type Item_Type           is private;
+      type Item_Pointer_Type   is access all Item_Type;
+      type Item_Reference_Type is not null access all Item_Type;
 
-      function Initialized_Object return Qbject_Type;
+      function Initialized_Object return Queue_Type;
 
       function Initialized_Item_Object (Payload : Handler_Reference_Type)
-      return Ibject_Type;
+      return Item_Type;
 
       procedure Enqueue (
-         Obj : in out Qbject_Type;
-         Itm :        Ibject_Reference_Type);
+         Obj : in out Queue_Type;
+         Itm :        Item_Reference_Type);
 
-      procedure Dequeue (Obj : in out Qbject_Type);
+      procedure Dequeue (Obj : in out Queue_Type);
 
-      function Head (Obj : Qbject_Type) return Ibject_Pointer_Type;
+      function Head (Obj : Queue_Type) return Item_Pointer_Type;
 
-      function Item_Payload (Itm : Ibject_Reference_Type)
+      function Item_Payload (Itm : Item_Reference_Type)
       return Handler_Reference_Type;
 
-      function Item_Next (Itm : Ibject_Reference_Type)
-      return Ibject_Pointer_Type;
+      function Item_Next (Itm : Item_Reference_Type)
+      return Item_Pointer_Type;
 
       procedure Remove (
-         Obj : in out Qbject_Type;
-         Itm : Ibject_Reference_Type);
+         Obj : in out Queue_Type;
+         Itm : Item_Reference_Type);
 
    private
 
-      type Qbject_Type is record
-         Head : Ibject_Pointer_Type;
-         Tail : Ibject_Pointer_Type;
+      type Queue_Type is record
+         Head : Item_Pointer_Type;
+         Tail : Item_Pointer_Type;
       end record;
 
-      type Ibject_Type is record
-         Next    : Ibject_Pointer_Type;
+      type Item_Type is record
+         Next    : Item_Pointer_Type;
          Payload : Handler_Reference_Type;
       end record;
 
-      function Empty (Obj : Qbject_Type)
+      function Empty (Obj : Queue_Type)
       return Boolean;
 
    end Handler_Queue;
@@ -109,47 +134,47 @@ private
    --
    package Context_Queue is
 
-      type Qbject_Type           is private;
-      type Ibject_Type           is private;
-      type Ibject_Pointer_Type   is access all Ibject_Type;
-      type Ibject_Reference_Type is not null access all Ibject_Type;
+      type Queue_Type           is private;
+      type Item_Type           is private;
+      type Item_Pointer_Type   is access all Item_Type;
+      type Item_Reference_Type is not null access all Item_Type;
 
-      function Initialized_Object return Qbject_Type;
+      function Initialized_Object return Queue_Type;
 
       function Initialized_Item_Object (Payload : Context_Reference_Type)
-      return Ibject_Type;
+      return Item_Type;
 
       procedure Enqueue (
-         Obj : in out Qbject_Type;
-         Itm :        Ibject_Reference_Type);
+         Obj : in out Queue_Type;
+         Itm :        Item_Reference_Type);
 
-      procedure Dequeue (Obj : in out Qbject_Type);
+      procedure Dequeue (Obj : in out Queue_Type);
 
-      function Head (Obj : Qbject_Type) return Ibject_Pointer_Type;
+      function Head (Obj : Queue_Type) return Item_Pointer_Type;
 
-      function Item_Payload (Itm : Ibject_Reference_Type)
+      function Item_Payload (Itm : Item_Reference_Type)
       return Context_Reference_Type;
 
-      function Item_Next (Itm : Ibject_Reference_Type)
-      return Ibject_Pointer_Type;
+      function Item_Next (Itm : Item_Reference_Type)
+      return Item_Pointer_Type;
 
       procedure Remove (
-         Obj : in out Qbject_Type;
-         Itm : Ibject_Reference_Type);
+         Obj : in out Queue_Type;
+         Itm : Item_Reference_Type);
 
    private
 
-      type Qbject_Type is record
-         Head : Ibject_Pointer_Type;
-         Tail : Ibject_Pointer_Type;
+      type Queue_Type is record
+         Head : Item_Pointer_Type;
+         Tail : Item_Pointer_Type;
       end record;
 
-      type Ibject_Type is record
-         Next    : Ibject_Pointer_Type;
+      type Item_Type is record
+         Next    : Item_Pointer_Type;
          Payload : Context_Reference_Type;
       end record;
 
-      function Empty (Obj : Qbject_Type)
+      function Empty (Obj : Queue_Type)
       return Boolean;
 
    end Context_Queue;
@@ -158,13 +183,9 @@ private
    type Context_Killer_Pointer_Type is access all Context_Killer_Type;
    type Context_Pointer_Type        is access all Context_Type;
 
-   type Receiver_Type is record
-      Handlers : Handler_Queue.Qbject_Type;
-   end record;
-
    type Handler_Type is record
       Thread     : CPP_Thread.Object_Reference_Type;
-      Queue_Item : aliased Handler_Queue.Ibject_Type;
+      Queue_Item : aliased Handler_Queue.Item_Type;
       Receiver   : Receiver_Pointer_Type;
    end record;
 
@@ -174,14 +195,20 @@ private
    end record;
 
    type Context_Type is record
-      Deliver_Queue_Item : Context_Queue.Ibject_Type;
-      Context_Queue_Item : Context_Queue.Ibject_Type;
-      Receiver           : Receiver_Reference_Type;
-      Imprint            : Imprint_Type;
-      Killer             : Context_Killer_Pointer_Type;
-      Nr_Of_Submits      : Number_Of_Submits_Type;
-      Acknowledged       : Boolean;
-      Killed             : Boolean;
+      Deliver_Item  : Context_Queue.Item_Type;
+      Contexts_Item : Context_Queue.Item_Type;
+      Receiver      : Receiver_Reference_Type;
+      Imprint       : Imprint_Type;
+      Killer        : Context_Killer_Pointer_Type;
+      Nr_Of_Submits : Number_Of_Submits_Type;
+      Acknowledged  : Boolean;
+      Killed        : Boolean;
+   end record;
+
+   type Receiver_Type is record
+      Handlers : Handler_Queue.Queue_Type;
+      Deliver  : Context_Queue.Queue_Type;
+      Contexts : Context_Queue.Queue_Type;
    end record;
 
 end Signal;
