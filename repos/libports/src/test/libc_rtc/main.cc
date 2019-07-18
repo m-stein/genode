73ee5c9f16c81f1c8d635ba1ecc24fb9fbece0d6
Martin Stein <martin.stein@genode-labs.com>
2019-07-18 19:20:22 +0200

    test-libc_rtc 1

---
+++ repos/libports/src/test/libc_rtc/main.cc
@@ -0,0 +1,21 @@
+/*
+ * \brief  pcsc-lite test
+ * \author Christian Prochaska
+ * \date   2016-09-19
+ */
+
+/*
+ * Copyright (C) 2016-2017 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Well, well\n");
+
+	return 0;
+}
