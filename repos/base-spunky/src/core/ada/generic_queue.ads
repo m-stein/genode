967f21291b95f72f4e677b21c7c8d95035cd809d
Martin Stein <martin.stein@genode-labs.com>
2019-12-23 22:57:21 +0100

    base-spunky: headers in generic queue files

--- repos/base-spunky/src/core/ada/generic_queue.ads
+++ repos/base-spunky/src/core/ada/generic_queue.ads
@@ -1,4 +1,18 @@
+--
+--  \brief  Generic FIFO-list data structure
+--  \author Martin stein
+--  \date   2019-04-24
+--
+
+--
+--  Copyright (C) 2019 Genode Labs GmbH
+--
+--  This file is part of the Genode OS framework, which is distributed
+--  under the terms of the GNU Affero General Public License version 3.
+--
+
 pragma Ada_2012;
+
 generic
    type Payload_Type;
    type Payload_Reference_Type is not null access all Payload_Type;
