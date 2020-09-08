17c18d3df292b6147092217a91d158bc3691f85f
Martin Stein <martin.stein@genode-labs.com>
2020-09-08 14:59:19 +0200

    timer_accuracy as cpu benchmark

--- repos/base/src/test/timer_accuracy/main.cc
+++ repos/base/src/test/timer_accuracy/main.cc
@@ -32,8 +32,37 @@ struct Main
 		log("");
 	}
 
+int xmain (void) __attribute__((aligned(4096))) {
+	unsigned volatile x { 2 };
+	for (unsigned volatile i = 2; i <= 400000; i++) {
+		bool prime_nr { true };
+		for (unsigned volatile j = 2; j < i; j++) {
+			unsigned volatile k = i / j;
+			unsigned volatile l = k * j;
+			if (i == l) {
+				prime_nr = false;
+				break;
+			}
+		}
+		if (prime_nr) {
+			x = i;
+		}
+	}
+	return x;
+}
+
 	Main(Env &env) : timer(env), timer_handler(env.ep(), *this, &Main::handle_timer)
 	{
+		while (1) {
+			Genode::log("sleep 50 sec");
+			timer.msleep(50000);
+			Genode::log("measure");
+			unsigned long long a = timer.elapsed_us();
+			xmain();
+			unsigned long long b = timer.elapsed_us();
+			Genode::log("result: ", (b-a)/1000, " ms");
+		}
+
 		timer.sigh(timer_handler);
 		handle_timer();
 	}
