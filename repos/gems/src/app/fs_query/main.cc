2517842650e395b60c967cbd0a60f63575ad4377
Martin Stein <martin.stein@genode-labs.com>
2021-03-25 15:15:45 +0100

    fs_query: gracefully deal with missing directory

--- repos/gems/src/app/fs_query/main.cc
+++ repos/gems/src/app/fs_query/main.cc
@@ -221,22 +221,27 @@ struct Fs_query::Main : Vfs::Watch_response_handler
 
 	void _handle_config()
 	{
-		_config.update();
+		try {
+			_config.update();
 
-		Xml_node const config = _config.xml();
+			Xml_node const config = _config.xml();
 
-		_root_dir_fs.apply_config(config.sub_node("vfs"));
+			_root_dir_fs.apply_config(config.sub_node("vfs"));
 
-		_dirs.for_each([&] (Registered<Watched_directory> &dir) {
-			destroy(_heap, &dir); });
+			_dirs.for_each([&] (Registered<Watched_directory> &dir) {
+				destroy(_heap, &dir); });
 
-		config.for_each_sub_node("query", [&] (Xml_node query) {
-			Directory::Path const path = query.attribute_value("path", Directory::Path());
-			new (_heap) Registered<Watched_directory>(_dirs, _heap, _root_dir, path, *this);
-		});
+			config.for_each_sub_node("query", [&] (Xml_node query) {
+				Directory::Path const path = query.attribute_value("path", Directory::Path());
+				new (_heap) Registered<Watched_directory>(_dirs, _heap, _root_dir, path, *this);
+			});
 
-		_reporter.generate([&] (Xml_generator &xml) {
-			_gen_listing(xml, config); });
+			_reporter.generate([&] (Xml_generator &xml) {
+				_gen_listing(xml, config); });
+		}
+		catch (Genode::Directory::Nonexistent_directory) {
+			_reporter.generate([&] (Xml_generator &) { });
+		}
 	}
 
 	Main(Env &env) : _env(env)
