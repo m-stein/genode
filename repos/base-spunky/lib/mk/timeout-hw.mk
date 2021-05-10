7a835bc9a9119244832dd1a79c44dd62cefe3711
Martin Stein <martin.stein@genode-labs.com>
2021-05-10 12:58:17 +0200

    spunky: re-enable 'make run/timeout' with staging

---
+++ repos/base-spunky/lib/mk/timeout-hw.mk
@@ -0,0 +1,8 @@
+INC_DIR += $(BASE_DIR)/../base-hw/src/include
+INC_DIR += $(BASE_DIR)/../base-hw/include
+INC_DIR += $(BASE_DIR)/../base-hw/include/spec/x86_64
+
+vpath %.cc $(BASE_DIR)/../base-hw/src/lib/base/x86_64
+vpath %.cc $(BASE_DIR)/../base-hw/src/lib/base/
+
+include $(BASE_DIR)/../base-hw/lib/mk/timeout-hw.mk
