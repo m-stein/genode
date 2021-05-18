6d76c3f4582e80df2409b3f7a805a0b0d018583a
Martin Stein <martin.stein@genode-labs.com>
2021-05-18 13:21:40 +0200

    squash "fs_query: gracefully deal with missing directory" (fix bug, enhance test)
    Please rewrite commit message:
    
    fs_query: gracefully deal with missing directories
    
    The fs_query component used to exit with an uncaught exception if a queried
    directory didn't exist. Now, fs_query will catch this event and simply skip the
    affected query, thereby indicating to the user the inexistence of the
    queried directory.
    
    Ref #4032

--- repos/gems/src/app/fs_query/main.cc
+++ repos/gems/src/app/fs_query/main.cc
@@ -223,7 +223,6 @@ struct Fs_query::Main : Vfs::Watch_response_handler
 
 	void _handle_config()
 	{
-		try {
 			_config.update();
 
 			Xml_node const config = _config.xml();
@@ -235,15 +234,16 @@ struct Fs_query::Main : Vfs::Watch_response_handler
 
 			config.for_each_sub_node("query", [&] (Xml_node query) {
 				Directory::Path const path = query.attribute_value("path", Directory::Path());
-				new (_heap) Registered<Watched_directory>(_dirs, _heap, _root_dir, path, *this);
+				try {
+					new (_heap)
+						Registered<Watched_directory>(
+							_dirs, _heap, _root_dir, path, *this);
+				}
+				catch (Genode::Directory::Nonexistent_directory) { }
 			});
 
 			_reporter.generate([&] (Xml_generator &xml) {
 				_gen_listing(xml, config); });
-		}
-		catch (Genode::Directory::Nonexistent_directory) {
-			_reporter.generate([&] (Xml_generator &) { });
-		}
 	}
 
 	Main(Env &env) : _env(env)
