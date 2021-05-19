51e52fe2dbd620a53042ee0edac0380c92b3ce01
Martin Stein <martin.stein@genode-labs.com>
2021-05-19 14:29:38 +0200

    fixup "rump: write less to the log" (fix compiler warning)
    Ref #4032

--- repos/dde_rump/src/lib/vfs/rump/vfs_rump.cc
+++ repos/dde_rump/src/lib/vfs/rump/vfs_rump.cc
@@ -441,13 +441,6 @@ class Vfs::Rump_file_system : public File_system
 				Genode::error("Mounting '",fs_type,"' file system failed (",errno,")");
 				throw Genode::Exception();
 			}
-			struct statvfs stats;
-			int err = rump_sys_statvfs1("/", &stats, ST_WAIT);
-			if (err == 0) {
-				double factor = 1.0 / (1<<30);
-				double available = factor * stats.f_bsize * stats.f_bavail;
-				double total = factor * stats.f_bsize * stats.f_blocks;
-			}
 		}
 
 		/***************************
