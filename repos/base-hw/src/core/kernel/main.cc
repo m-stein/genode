20d77a35bd666283bc3d539def538bb0b85724b8
Martin Stein <martin.stein@genode-labs.com>
2021-07-12 17:26:32 +0200

    base-hw: don't use boot_info() outside main.cc
    It's sufficient to access the boot info only on kernel initialization time.
    Therfore, it can remain completely hidden to the rest of the kernel inside
    kernel/main.cc in the initialization function.
    
    Ref #4217

--- repos/base-hw/src/core/kernel/main.cc
+++ repos/base-hw/src/core/kernel/main.cc
@@ -37,12 +37,14 @@ class Kernel::Main
 		static Main *_instance;
 
 		Lock                                    _data_lock        { };
-		Cpu_pool                                _cpu_pool         { };
+		Cpu_pool                                _cpu_pool;
 		Genode::Constructible<Core_main_thread> _core_main_thread { };
 
 		void _handle_kernel_entry();
 
 	public:
+
+		Main(unsigned nr_of_cpus);
 
 		static void handle_kernel_entry();
 
@@ -53,6 +55,12 @@ class Kernel::Main
 
 
 Kernel::Main *Kernel::Main::_instance;
+
+
+Kernel::Main::Main(unsigned nr_of_cpus)
+:
+	_cpu_pool { nr_of_cpus }
+{ }
 
 
 void Kernel::Main::_handle_kernel_entry()
@@ -78,11 +86,17 @@ void Kernel::Main::handle_kernel_entry()
 
 void Kernel::Main::initialize_and_handle_kernel_entry()
 {
-	static volatile bool instance_initialized = false;
-	static volatile bool pool_ready           = false;
-	static volatile bool kernel_ready         = false;
+	using Boot_info = Hw::Boot_info<Board::Boot_info>;
 
-	bool const primary_cpu { Cpu::executing_id() == Cpu::primary_id() };
+	static volatile bool     instance_initialized   { false };
+	static volatile unsigned nr_of_initialized_cpus { 0 };
+	static volatile bool     kernel_initialized     { false };
+
+	Boot_info &boot_info {
+		*reinterpret_cast<Boot_info*>(Hw::Mm::boot_info().base) };
+
+	unsigned const nr_of_cpus  { boot_info.cpus };
+	bool     const primary_cpu { Cpu::executing_id() == Cpu::primary_id() };
 
 	if (primary_cpu) {
 
@@ -90,7 +104,7 @@ void Kernel::Main::initialize_and_handle_kernel_entry()
 		 * Let the primary CPU create a Main object and initialize the static
 		 * reference to it.
 		 */
-		static Main instance { };
+		static Main instance { nr_of_cpus };
 		_instance = &instance;
 
 	} else {
@@ -103,41 +117,51 @@ void Kernel::Main::initialize_and_handle_kernel_entry()
 	}
 
 	{
+		/**
+		 * Let each CPU initialize its corresponding CPU object in the
+		 * CPU pool.
+		 */
 		Lock::Guard guard(_instance->_data_lock);
-
 		instance_initialized = true;
-
-		/* initialize current cpu */
-		pool_ready = _instance->_cpu_pool.initialize();
+		_instance->_cpu_pool.initialize_executing_cpu();
+		nr_of_initialized_cpus++;
 	};
 
-	/* wait until all cpus have initialized their corresponding cpu object */
-	while (!pool_ready) { ; }
+	/**
+	 * Let all CPUs block until each CPU object in the CPU pool has been
+	 * initialized by the corresponding CPU.
+	 */
+	while (nr_of_initialized_cpus < nr_of_cpus) { }
 
-	/* the boot-cpu initializes the rest of the kernel */
 	if (primary_cpu) {
-		Lock::Guard guard(_instance->_data_lock);
 
-		using Boot_info = Hw::Boot_info<Board::Boot_info>;
-		Boot_info &boot_info {
-			*reinterpret_cast<Boot_info*>(Hw::Mm::boot_info().base) };
+		/**
+		 * Let the primary CPU initialize the core main thread and finish
+		 * initialization of the boot info.
+		 */
+
+		Lock::Guard guard(_instance->_data_lock);
 
 		_instance->_cpu_pool.for_each_cpu([&] (Kernel::Cpu &cpu) {
 			boot_info.kernel_irqs.add(cpu.timer().interrupt_id());
 		});
 		boot_info.kernel_irqs.add((unsigned)Board::Pic::IPI);
 
-		Genode::log("");
-		Genode::log("kernel initialized");
-
 		_instance->_core_main_thread.construct(_instance->_cpu_pool);
 		boot_info.core_main_thread_utcb =
 			(addr_t)_instance->_core_main_thread->utcb();
 
-		kernel_ready = true;
+		Genode::log("");
+		Genode::log("kernel initialized");
+		kernel_initialized = true;
+
 	} else {
-		/* secondary cpus spin until the kernel is initialized */
-		while (!kernel_ready) {;}
+
+		/**
+		 * Let secondary CPUs block until the primary CPU has initialized the
+		 * core main thread and finished initialization of the boot info.
+		 */
+		while (!kernel_initialized) {;}
 	}
 
 	_instance->_handle_kernel_entry();
