a9e91de37ef0a562a00d03f5392e7be6190f15c6
Martin Stein <martin.stein@genode-labs.com>
2021-07-15 12:57:36 +0200

    nic_router: uncaught exc. on handling link state
    An interface that received a signal for a link-state change accessed its
    domain reference without assuming that it could not be attached to a domain
    at that moment. This caused the NIC router to crash with an uncaught exception
    of type 'Net::Pointer<Net::Domain>::Invalid'. The commit adds a catch
    directive for this exception resulting in the handler doing nothing if not
    attached to any domain.
    
    Fixes #4222

--- repos/os/src/server/nic_router/interface.cc
+++ repos/os/src/server/nic_router/interface.cc
@@ -899,6 +899,7 @@ void Interface::handle_interface_link_state()
 			domain_.discard_ip_config();
 		}
 	}
+	catch (Pointer<Domain>::Invalid) { }
 	catch (Domain::Ip_config_static) { }
 	catch (Keep_ip_config) { }
 
