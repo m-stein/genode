3127b11e7f82cb1f9a39418740b2aafdcd64452d
Martin Stein <martin.stein@genode-labs.com>
2021-08-10 13:20:37 +0200

    fixup "Trace_buffer: do not reprocess last read entry" (doc)
    Ref #4244

--- repos/gems/src/lib/vfs/trace/trace_buffer.h
+++ repos/gems/src/lib/vfs/trace/trace_buffer.h
@@ -48,11 +48,21 @@ class Trace_buffer
 			Trace::Buffer::Entry curr { _curr };
 			Trace::Buffer::Entry e2   { _curr };
 
-			/** _curr is either uninitialised (invalid)
- 			 *  or marks the last processed entry.
- 			 *  In the former case, we start with the first entry.
- 			 *  In the latter case, we proceed with the next unprocessed entry.
- 			 */
+			/**
+			 * If '_curr' is marked 'last' (i.e., the entry pointer it contains
+			 * is invalid), either this method wasn't called before on this
+			 * buffer or the buffer was empty on all previous calls (note that
+			 * '_curr' is only updated with valid entry pointers by this
+			 * method). In this case, we start with the first entry (if any).
+			 *
+			 * If '_curr' is not marked 'last' it points to the last processed
+			 * entry and we proceed with the, so far unprocessed entry next to
+			 * it (if any). Note that in this case, the entry behind '_curr'
+			 * might have got overridden since the last call to this method
+			 * because the buffer wrapped and oustripped the entry consumer.
+			 * This problem is well known and should be avoided by choosing a
+			 * large enough buffer.
+			 */
 			if (e2.last())
 				e2 = _buffer.first();
 			else
