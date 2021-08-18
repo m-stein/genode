6bd703afcd5ecdb8727271dc6b26f2ffe8b4dfa0
Martin Stein <martin.stein@genode-labs.com>
2021-08-18 16:30:29 +0200

    fixup "base-hw: always use 'unsigned' for priorities"

--- repos/base-hw/src/core/kernel/thread.h
+++ repos/base-hw/src/core/kernel/thread.h
@@ -317,7 +317,7 @@ class Kernel::Thread : private Kernel::Object, public Cpu_job, private Timeout
 		       char                 const *const  label)
 		:
 			Thread(addr_space_id_alloc, user_irq_pool, cpu_pool, core_pd,
-			       Cpu_priority::MIN, 0, label, true)
+			       Cpu_priority::min(), 0, label, true)
 		{ }
 
 		~Thread();
