ec475780d83e6edaa2c6562a253199c7a92a7e89
Martin Stein <martin.stein@genode-labs.com>
2022-01-06 15:34:17 +0100

    wireguard: fix bug in wireguard_x86_64_linux.mk

--- repos/dde_linux/lib/mk/wireguard_x86_64_linux.mk
+++ repos/dde_linux/lib/mk/wireguard_x86_64_linux.mk
@@ -4,13 +4,14 @@
 # output of the Linux build).
 #
 
-LX_SRC_DIR := $(BUILD_BASE_DIR)/server/wireguard/linux
-LX_OUT_DIR := $(BUILD_BASE_DIR)/server/wireguard/linux
+LX_CONTRIB_DIR := $(call select_from_ports,linux)/src/linux
+LX_SRC_DIR     := $(BUILD_BASE_DIR)/server/wireguard/linux
+LX_OUT_DIR     := $(BUILD_BASE_DIR)/server/wireguard/linux
 
 $(LX_OUT_DIR):
 	$(VERBOSE)rm -rf $(LX_SRC_DIR)
 	$(VERBOSE)mkdir -p $(LX_SRC_DIR)
-	$(VERBOSE)cp -r $(call select_from_ports,linux)/src/linux/* $(LX_SRC_DIR)
+	$(VERBOSE)cp -r $(LX_CONTRIB_DIR)/* $(LX_SRC_DIR)
 
 include $(REP_DIR)/src/wg_linux/kernel_build_phony.inc
 
