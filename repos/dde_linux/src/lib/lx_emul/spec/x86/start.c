19111d8f85b437dc48430e5ded9d7d36e8d50f95
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-01-06 11:42:08 +0100

    dde_linux: split into architecural parts

---
+++ repos/dde_linux/src/lib/lx_emul/spec/x86/start.c
@@ -0,0 +1,19 @@
+/*
+ * \brief  Linux Kernel initialization
+ * \author Stefan Kalkowski
+ * \date   2022-01-08
+ */
+
+/*
+ * Copyright (C) 2021 Genode Labs GmbH
+ *
+ * This file is distributed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+#include <lx_emul/init.h>
+
+void time_init(void) {}
+
+
+void lx_emul_setup_arch(void *dtb) { }
