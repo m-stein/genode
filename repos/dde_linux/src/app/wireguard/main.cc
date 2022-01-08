38f143d81cf25b9722b4550ccbde9a77b1bcd5ef
Martin Stein <martin.stein@genode-labs.com>
2022-01-08 09:33:24 +0100

    wireguard: coding style in main.cc

--- repos/dde_linux/src/app/wireguard/main.cc
+++ repos/dde_linux/src/app/wireguard/main.cc
@@ -1,40 +1,61 @@
 /*
  * \brief  Wireguard component
  * \author Stefan Kalkowski
+ * \author Martin Stein
  * \date   2022-01-07
  */
 
 /*
- * Copyright (C) 2021 Genode Labs GmbH
+ * Copyright (C) 2022 Genode Labs GmbH
  *
  * This file is part of the Genode OS framework, which is distributed
  * under the terms of the GNU Affero General Public License version 3.
  */
 
+/* base includes */
 #include <base/component.h>
+
+/* lx-kit includes */
 #include <lx_kit/env.h>
 #include <lx_emul/init.h>
 
 using namespace Genode;
 
-/** Dummy device list implementation replacement for lx_kit **/
-Lx_kit::Device_list::Device_list(Entrypoint&, Heap&, Platform::Connection & p)
-: _platform(p) {}
+namespace Wireguard {
+
+	class Main;
+}
 
 
-struct Main
+/*
+ * Dummy device list implementation replacement for lx_kit
+ */
+Lx_kit::Device_list::Device_list(Entrypoint           &,
+                                 Heap                 &,
+                                 Platform::Connection &platform)
+:
+	_platform { platform }
+{ }
+
+
+class Wireguard::Main
 {
-	Env & env;
+	private:
 
-	Main(Env & env) : env(env)
+		Env &_env;
+
+
+	public:
+
+		Main(Env &env) : _env(env)
 	{
-		Lx_kit::initialize(env);
+		Lx_kit::initialize(_env);
 		lx_emul_start_kernel(nullptr);
 	}
 };
 
 
-void Component::construct(Env & env)
+void Component::construct(Env &env)
 {
-	static Main main(env);
+	static Wireguard::Main main { env };
 }
