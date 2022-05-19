11ed5b98cf81a96cbee9f6fdb689ed212da18522
Martin Stein <martin.stein@genode-labs.com>
2022-05-19 18:12:20 +0200

    lx_emul/random: reseed the sponge regularly
    With this commit, the Xoroshiro sponge in lx_emul/random.cc gets re-seeded
    with jitterentropy every 1024 * 1024 + random(0..255) bytes of generated
    output.
    
    Ref #4397

--- repos/dde_linux/src/lib/lx_emul/random.cc
+++ repos/dde_linux/src/lib/lx_emul/random.cc
@@ -38,54 +38,6 @@
 
 using namespace Genode;
 
-/*
- * Xoroshiro128+ written in 2014-2016 by Sebastiano Vigna (vigna@acm.org)
- *
- * (see http://xoroshiro.di.unimi.it/xorshift128plus.c and
- *      http://xoroshiro.di.unimi.it/splitmix64.c)
- */
-class Xoroshiro_128_plus
-{
-	private:
-
-		uint64_t _seed;
-		uint64_t _s[2];
-
-		uint64_t _splitmix64()
-		{
-			uint64_t z = (_seed += __UINT64_C(0x9E3779B97F4A7C15));
-			z = (z ^ (z >> 30)) * __UINT64_C(0xBF58476D1CE4E5B9);
-			z = (z ^ (z >> 27)) * __UINT64_C(0x94D049BB133111EB);
-			return z ^ (z >> 31);
-		}
-
-		static uint64_t _rotl(uint64_t const x, int k)
-		{
-			return (x << k) | (x >> (64 - k));
-		}
-
-	public:
-
-		Xoroshiro_128_plus(uint64_t seed) : _seed(seed)
-		{
-			_s[0] = _splitmix64();
-			_s[1] = _splitmix64();
-		}
-
-		uint64_t get_u64()
-		{
-			uint64_t const s0 = _s[0];
-			uint64_t       s1 = _s[1];
-			uint64_t const result = s0 + s1;
-
-			s1 ^= s0;
-
-			_s[0] = _rotl(s0, 55) ^ s1 ^ (s1 << 14);
-			_s[1] = _rotl(s1, 36);
-
-			return result;
-		}
-};
 
 static uint64_t jitterentropy_gen_random_u64()
 {
@@ -107,9 +59,80 @@ static uint64_t jitterentropy_gen_random_u64()
 }
 
 
+/*
+ * Xoroshiro128+ written in 2014-2016 by Sebastiano Vigna (vigna@acm.org)
+ * plus a self-written automatic re-seeding every X + random(0..255) bytes of
+ * generated output.
+ *
+ * (see http://xoroshiro.di.unimi.it/xorshift128plus.c and
+ *      http://xoroshiro.di.unimi.it/splitmix64.c)
+ */
+class Xoroshiro_128_plus
+{
+	private:
+
+		enum { NR_OF_GEN_BYTES_BASE_LIMIT = 1024 * 1024 };
+
+		uint64_t _seed                  { 0 };
+		size_t   _nr_of_gen_bytes       { 0 };
+		size_t   _nr_of_gen_bytes_limit { 0 };
+		uint64_t _s[2];
+
+		void _gen_seed()
+		{
+			_seed = jitterentropy_gen_random_u64();
+			_nr_of_gen_bytes = 0;
+			_nr_of_gen_bytes_limit =
+				NR_OF_GEN_BYTES_BASE_LIMIT + (_seed & 0xff);
+		}
+
+		uint64_t _splitmix64()
+		{
+			uint64_t z = (_seed += __UINT64_C(0x9E3779B97F4A7C15));
+			z = (z ^ (z >> 30)) * __UINT64_C(0xBF58476D1CE4E5B9);
+			z = (z ^ (z >> 27)) * __UINT64_C(0x94D049BB133111EB);
+			return z ^ (z >> 31);
+		}
+
+		static uint64_t _rotl(uint64_t const x, int k)
+		{
+			return (x << k) | (x >> (64 - k));
+		}
+
+	public:
+
+		Xoroshiro_128_plus()
+		{
+			_gen_seed();
+			_s[0] = _splitmix64();
+			_s[1] = _splitmix64();
+		}
+
+		uint64_t get_u64()
+		{
+			/* consider re-seeding */
+			_nr_of_gen_bytes += 8;
+			if (_nr_of_gen_bytes > _nr_of_gen_bytes_limit) {
+				_gen_seed();
+			}
+
+			uint64_t const s0 = _s[0];
+			uint64_t       s1 = _s[1];
+			uint64_t const result = s0 + s1;
+
+			s1 ^= s0;
+
+			_s[0] = _rotl(s0, 55) ^ s1 ^ (s1 << 14);
+			_s[1] = _rotl(s1, 36);
+
+			return result;
+		}
+};
+
+
 static Xoroshiro_128_plus &xoroshiro()
 {
-	static Xoroshiro_128_plus xoroshiro { jitterentropy_gen_random_u64() };
+	static Xoroshiro_128_plus xoroshiro { };
 	return xoroshiro;
 }
 
