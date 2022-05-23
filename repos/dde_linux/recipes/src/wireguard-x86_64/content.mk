ffd99ea057c564b7c1e459fbf53d5dd481e818cd
Martin Stein <martin.stein@genode-labs.com>
2022-05-23 09:46:13 +0200

    fixup "wireguard: sculpt integration" (bin/x86_64/wireguard builds 2)

--- repos/dde_linux/recipes/src/wireguard-x86_64/content.mk
+++ repos/dde_linux/recipes/src/wireguard-x86_64/content.mk
@@ -10,8 +10,7 @@ MIRRORED_FROM_REP_DIR := \
 	src/app/wireguard \
 	src/lib/lx_emul \
 	src/lib/lx_kit \
-	src/include/spec/x86_64 \
-	src/include/spec/x86 \
+	src/include/spec/x86_64/lx_kit \
 	src/include/lx_emul \
 	src/include/lx_kit \
 	src/include/lx_user
@@ -22,10 +21,13 @@ $(MIRRORED_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
 
 
-##############################################
-## The following code was copied from the   ##
-## repos/pc/recipes/api/pc_linux/content.mk ##
-##############################################
+#
+# FIXME
+#
+# The following code was copied from the recipe
+# repos/pc/recipes/api/pc_linux/content.mk. It should be replaced by
+# creating a new api/virt_linux recipe and adding it to wireguard/used_apis.
+#
 
 #
 # Content from the Linux source tree
@@ -102,10 +104,6 @@ LX_FILES += Kbuild \
             tools/include/tools \
             tools/objtool
 
-# needed for src/asn1_compiler.c
-LX_FILES += include/linux/asn1.h \
-            include/linux/asn1_ber_bytecode.h
-
 LX_SCRIPTS_KCONFIG_FILES := $(notdir $(wildcard $(LX_ABS_DIR)/scripts/kconfig/*.c)) \
                             $(notdir $(wildcard $(LX_ABS_DIR)/scripts/kconfig/*.h)) \
                             Makefile lexer.l parser.y
@@ -114,66 +112,16 @@ LX_FILES += $(addprefix scripts/kconfig/,$(LX_SCRIPTS_KCONFIG_FILES)) \
 LX_FILES += $(shell cd $(LX_ABS_DIR); find -name "Kconfig*" -printf "%P\n")
 
 # needed for generated/asm-offsets.h
-LX_FILES += arch/x86/include/asm/boot.h \
-            arch/x86/include/asm/cpufeature.h \
-            arch/x86/include/asm/current.h \
-            arch/x86/include/asm/fixmap.h \
-            arch/x86/include/asm/fpu/api.h \
-            arch/x86/include/asm/fpu/xstate.h \
-            arch/x86/include/asm/ia32.h \
-            arch/x86/include/asm/irqflags.h \
-            arch/x86/include/asm/nospec-branch.h \
-            arch/x86/include/asm/page.h \
-            arch/x86/include/asm/page_32.h \
-            arch/x86/include/asm/page_32_types.h \
-            arch/x86/include/asm/pgtable.h \
-            arch/x86/include/asm/pgtable-2level.h \
-            arch/x86/include/asm/pgtable-2level_types.h \
-            arch/x86/include/asm/pgtable_32.h \
-            arch/x86/include/asm/pgtable_32_areas.h \
-            arch/x86/include/asm/pgtable_32_types.h \
-            arch/x86/include/asm/pgtable_64.h \
-            arch/x86/include/asm/pgtable-invert.h \
-            arch/x86/include/asm/pkru.h \
-            arch/x86/include/asm/sigframe.h \
-            arch/x86/include/asm/suspend.h \
-            arch/x86/include/asm/suspend_32.h \
-            arch/x86/include/asm/suspend_64.h \
-            arch/x86/include/asm/sync_core.h \
-            arch/x86/include/asm/uaccess_32.h \
-            arch/x86/include/asm/user_32.h \
-            arch/x86/include/uapi/asm/ucontext.h \
-            arch/x86/kernel/asm-offsets.c \
-            arch/x86/kernel/asm-offsets_32.c \
+LX_FILES += arch/x86/kernel/asm-offsets.c \
             arch/x86/kernel/asm-offsets_64.c \
-            include/acpi \
             include/asm-generic/current.h \
             include/asm-generic/fixmap.h \
             include/asm-generic/memory_model.h \
             include/asm-generic/pgtable_uffd.h \
             include/asm-generic/pgtable-nopmd.h \
-            include/asm-generic/pgtable-nopud.h \
-            include/asm-generic/qspinlock.h \
-            include/linux/arm-smccc.h \
-            include/linux/arm_sdei.h \
-            include/linux/cper.h \
-            include/linux/crypto.h \
-            include/linux/efi.h \
-            include/linux/pgtable.h \
-            include/linux/pstore.h \
-            include/uapi/asm-generic/ucontext.h \
-            include/uapi/linux/arm_sdei.h \
             kernel/bounds.c \
             kernel/time/timeconst.bc
 
-# needed for gen_crc32table
-LX_FILES += lib/gen_crc32table.c \
-            lib/crc32.c
-
-content: src/linux/include/linux/kvm_host.h
-src/linux/include/linux/kvm_host.h: # cut dependencies from kvm via dummy header
-	mkdir -p $(dir $@)
-	touch $@
 
 # add content listed in the repository's source.list or dep.list files
 LX_FILE_LISTS := $(shell find -H $(REP_DIR) -name dep.list -or -name source.list)
