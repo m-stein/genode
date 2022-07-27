90a07f5637ec5f3aad08aca47fa923d4ea2d1bf3
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2022-07-27 12:11:17 +0200

    Revert "nic_router: incremental L4 checksum updates"
    This reverts commit 24a2151c5a2c72a2b9b19bf440ac4be8d0f29c94.

--- repos/os/include/net/ipv4.h
+++ repos/os/include/net/ipv4.h
@@ -98,9 +98,6 @@ class Net::Ipv4_packet
 
 		void update_checksum(Internet_checksum_diff const &icd);
 
-		void update_checksum(Internet_checksum_diff const &icd,
-		                     Internet_checksum_diff       &caused_icd);
-
 		bool checksum_error() const;
 
 	private:
