564a5187650fe7a4a57da36ac0a245f3fd0cfcf3
Martin Stein <martin.stein@genode-labs.com>
2022-12-19 11:26:43 +0100

    base-hw ipc: no _announce_request

--- repos/base-hw/src/core/kernel/ipc_node.h
+++ repos/base-hw/src/core/kernel/ipc_node.h
@@ -56,11 +56,6 @@ class Kernel::Ipc_node
 		void _receive_from(Ipc_node &node);
 
 		/**
-		 * Insert 'r' into request queue, buffer it if we were waiting for it
-		 */
-		void _announce_request(Ipc_node &node);
-
-		/**
 		 * Cancel an ongoing send operation
 		 */
 		void _cancel_send();
