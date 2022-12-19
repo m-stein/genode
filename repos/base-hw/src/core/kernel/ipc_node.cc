83574ef783bd1e189138d42868b9dfac60696506
Martin Stein <martin.stein@genode-labs.com>
2022-12-19 11:52:49 +0100

    base-hw ipc: rename callee

--- repos/base-hw/src/core/kernel/ipc_node.cc
+++ repos/base-hw/src/core/kernel/ipc_node.cc
@@ -35,13 +35,13 @@ void Ipc_node::_receive_from(Ipc_node &node)
 
 void Ipc_node::_cancel_send()
 {
-	if (_callee) {
-		if (_callee->_caller == this) {
-			_callee->_caller = nullptr;
+	if (_out_node) {
+		if (_out_node->_caller == this) {
+			_out_node->_caller = nullptr;
 		} else {
-			_callee->_request_queue.remove(_request_queue_item);
+			_out_node->_request_queue.remove(_request_queue_item);
 		}
-		_callee = nullptr;
+		_out_node = nullptr;
 	}
 	if (_out_sending()) {
 		_thread.ipc_send_request_failed();
@@ -62,17 +62,17 @@ bool Ipc_node::can_send_request() const
 }
 
 
-void Ipc_node::send_request(Ipc_node &callee, bool help)
+void Ipc_node::send_request(Ipc_node &node, bool help)
 {
 	_state    = AWAIT_REPLY;
-	_callee   = &callee;
+	_out_node = &node;
 	_help     = false;
 
-	if (_callee->_in_waiting()) {
-		_callee->_receive_from(*this);
-		_callee->_thread.ipc_await_request_succeeded();
+	if (_out_node->_in_waiting()) {
+		_out_node->_receive_from(*this);
+		_out_node->_thread.ipc_await_request_succeeded();
 	} else {
-		_callee->_request_queue.enqueue(_request_queue_item);
+		_out_node->_request_queue.enqueue(_request_queue_item);
 	}
 	_help = help;
 }
@@ -80,7 +80,7 @@ void Ipc_node::send_request(Ipc_node &callee, bool help)
 
 Thread &Ipc_node::helping_sink()
 {
-	return _helping() ? _callee->helping_sink() : _thread;
+	return _helping() ? _out_node->helping_sink() : _thread;
 }
 
 
@@ -135,8 +135,8 @@ Ipc_node::~Ipc_node()
 	_cancel_send();
 
 	if (_caller) {
-		if (_caller->_callee) {
-			_caller->_callee = nullptr;
+		if (_caller->_out_node) {
+			_caller->_out_node = nullptr;
 			_caller->_state  = INACTIVE;
 			_caller->_thread.ipc_send_request_failed();
 			_caller = nullptr;
@@ -144,8 +144,8 @@ Ipc_node::~Ipc_node()
 	}
 	_request_queue.dequeue_all([] (Queue_item &item) {
 		Ipc_node &node { item.object() };
-		if (node._callee) {
-			node._callee = nullptr;
+		if (node._out_node) {
+			node._out_node = nullptr;
 			node._state  = INACTIVE;
 			node._thread.ipc_send_request_failed();
 		}
