0ab5c00a5b72ff19af958436c4820efc30dc2438
Martin Stein <martin.stein@genode-labs.com>
2022-12-20 14:10:50 +0100

    base-hw ipc: get rid of caller member

--- repos/base-hw/src/core/kernel/ipc_node.h
+++ repos/base-hw/src/core/kernel/ipc_node.h
@@ -127,11 +127,6 @@ class Kernel::Ipc_node
 		 */
 		template <typename F> void for_each_helper(F f)
 		{
-			/* if we have a helper in the receive buffer, call 'f' for it */
-			if (_caller && _caller->_helping())
-				f(_caller->_thread);
-
-			/* call 'f' for each helper in our request queue */
 			_in_queue.for_each([f] (Queue_item &item) {
 				Ipc_node &node { item.object() };
 
