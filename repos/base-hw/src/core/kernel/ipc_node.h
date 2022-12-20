823bd27e6222271fbc58eca333978eab583ef575
Martin Stein <martin.stein@genode-labs.com>
2022-12-20 14:18:05 +0100

    Revert "fixup"
    This reverts commit 88b3a819617bf0adfbbcea3a08de53d6ef5c7ab7.

--- repos/base-hw/src/core/kernel/ipc_node.h
+++ repos/base-hw/src/core/kernel/ipc_node.h
@@ -34,6 +34,25 @@ class Kernel::Ipc_node
 
 		using Queue_item = Genode::Fifo_element<Ipc_node>;
 
+		struct Out
+		{
+			enum State
+			{
+				READY,
+				SEND,
+				SEND_HELPING,
+				DESTRUCT
+			};
+
+			State     state { READY   };
+			Ipc_node *node  { nullptr };
+
+			bool sending() const
+			{
+				return state == SEND_HELPING || state == SEND;
+			}
+		};
+
 		struct In
 		{
 			using Queue = Genode::Fifo<Queue_item>;
@@ -53,25 +72,6 @@ class Kernel::Ipc_node
 			bool waiting() const
 			{
 				return state == WAIT;
-			}
-		};
-
-		struct Out
-		{
-			enum State
-			{
-				READY,
-				SEND,
-				SEND_HELPING,
-				DESTRUCT
-			};
-
-			State     state { READY   };
-			Ipc_node *node  { nullptr };
-
-			bool sending() const
-			{
-				return state == SEND_HELPING || state == SEND;
 			}
 		};
 
