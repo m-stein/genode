0caa8ada22808705b7012d399facfdc24f32f565
Martin Stein <martin.stein@genode-labs.com>
2023-01-05 13:52:19 +0100

    Revert "fixup"
    This reverts commit d2e706d575f256cac6cacd15ed822b4ba6548ada.

--- repos/base-hw/src/core/kernel/ipc_node.h
+++ repos/base-hw/src/core/kernel/ipc_node.h
@@ -33,10 +33,11 @@ class Kernel::Ipc_node
 	private:
 
 		using Queue_item = Genode::Fifo_element<Ipc_node>;
-		using Queue      = Genode::Fifo<Queue_item>;
 
 		struct In
 		{
+			using Queue = Genode::Fifo<Queue_item>;
+
 			enum State
 			{
 				READY,
