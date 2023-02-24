445f3328b7eb1a97e69254df46c3225becabda78
Martin Stein <martin.stein@genode-labs.com>
2023-02-24 14:36:47 +0100

    tmp

--- repos/gems/src/app/cbe_tester/block_io.h
+++ repos/gems/src/app/cbe_tester/block_io.h
@@ -32,7 +32,9 @@ class Cbe::Block_io_request : public Module_request
 {
 	public:
 
-		enum Type { INVALID = 0, READ = 1, WRITE = 2, SYNC = 3 };
+		enum Type {
+			INVALID = 0, READ = 1, WRITE = 2, SYNC = 3, WRITE_CLIENT_DATA = 4,
+			READ_CLIENT_DATA = 5 };
 
 	private:
 
