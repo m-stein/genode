d57bf5f05711e39f3ebf4f61836942c3dad7c4c8
Martin Stein <martin.stein@genode-labs.com>
2023-07-27 15:43:58 +0200

    Revert "debug "tresor: add temporary debugging output""
    This reverts commit 75e29a6c6da5cf83d4161f674c090486ca0d3b92.

--- repos/gems/src/lib/tresor/block_io.cc
+++ repos/gems/src/lib/tresor/block_io.cc
@@ -624,31 +624,15 @@ bool Block_io::_peek_completed_request(uint8_t *buf_ptr,
 					calc_sha256_4k_hash(*(Block *)req._blk_ptr, hash);
 					log("block_io: ", req.type_name(), " pba ", req._pba,
 					    " data ", *(Block *)req._blk_ptr, " hash ", hash);
-					if (req._pba == 37) {
-/*
-log("--- data ---");
-log(Byte_range((uint8_t *)req._blk_ptr, 4096));
-log("------------");
-*/
-					}
 
 					break;
 				}
 				case Request::READ_CLIENT_DATA:
-				{
-					Hash hash;
-					calc_sha256_4k_hash(channel._blk_buf, hash);
-					log("block_io: read pba ", req._pba,
-					    " data ", channel._blk_buf,
-					    " hash ", hash);
-
-					break;
-				}
 				case Request::WRITE_CLIENT_DATA:
 				{
 					Hash hash;
 					calc_sha256_4k_hash(channel._blk_buf, hash);
-					log("block_io: write pba ", req._pba,
+					log("block_io: ", req.type_name(), " pba ", req._pba,
 					    " data ", channel._blk_buf,
 					    " hash ", hash);
 
