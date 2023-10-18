092f4fe8374a5017b1e7bff47aaf60e3894cf392
Martin Stein <martin.stein@genode-labs.com>
2023-10-18 15:12:37 +0200

    gems: file_vault_23_04

---
+++ repos/gems/src/app/file_vault_23_04/sync_to_cbe_vfs_init/target.mk
@@ -0,0 +1,3 @@
+TARGET = file_vault_23_04-sync_to_cbe_vfs_init
+LIBS   = base vfs
+SRC_CC = main.cc
---
+++ repos/gems/src/app/file_vault_23_04/truncate_file/target.mk
@@ -0,0 +1,4 @@
+TARGET  := file_vault_23_04-truncate_file
+SRC_CC  += main.cc
+INC_DIR += $(PRG_DIR)/..
+LIBS    += base vfs
