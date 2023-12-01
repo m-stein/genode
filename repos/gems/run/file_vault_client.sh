53d6bc23115b1574f9af4a3e41c2a42190489956
Martin Stein <martin.stein@genode-labs.com>
2023-12-01 12:52:05 +0100

    debug "gems: add autopilot test 'file_vault_client'" (wait for rump sync)

--- repos/gems/run/file_vault_client.sh
+++ repos/gems/run/file_vault_client.sh
@@ -13,4 +13,8 @@ echo "Und Sonderzeichen: /§($)=%!" >> file_vault/dir_1/file_2
 cat file_vault/dir_1/file_2
 ls -la file_vault/
 ls -la file_vault/dir_1
+
+# a dirty fix that should ensure that rump fs is synchronizing before we leave
+wait 5
+
 exit 0
