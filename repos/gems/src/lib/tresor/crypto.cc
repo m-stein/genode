576909019ab8294be04e522155611cf50b67cea4
Martin Stein <martin.stein@genode-labs.com>
2023-12-01 21:23:12 +0100

    fixup "tresor: improved module framework and clean-up" (fix VERBOSE_BLOCK_IO)
    Ref #5062

--- repos/gems/src/lib/tresor/crypto.cc
+++ repos/gems/src/lib/tresor/crypto.cc
@@ -115,6 +115,17 @@ void Crypto_channel::_mark_req_successful(bool &progress)
 		default: break;
 		}
 	}
+	if (VERBOSE_BLOCK_IO && (!VERBOSE_BLOCK_IO_PBA_FILTER || VERBOSE_BLOCK_IO_PBA == req._pba)) {
+		switch (req._type) {
+		case Request::DECRYPT_CLIENT_DATA:
+			log("block_io: read pba ", req._pba, " hash ", hash(req._blk), " (plaintext hash ", hash(_blk), ")");
+			break;
+		case Request::ENCRYPT_CLIENT_DATA:
+			log("block_io: write pba ", req._pba, " hash ", hash(req._blk), " (plaintext hash ", hash(_blk), ")");
+			break;
+		default: break;
+		}
+	}
 }
 
 
