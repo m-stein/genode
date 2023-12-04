e8fcd740ff4241f68ad8374298be610b4f5991dc
Martin Stein <martin.stein@genode-labs.com>
2023-12-04 10:06:33 +0100

    debug

--- repos/gems/src/lib/tresor/superblock_control.cc
+++ repos/gems/src/lib/tresor/superblock_control.cc
@@ -496,7 +496,7 @@ void Superblock_control_channel::_initialize(bool &progress)
 
 		_sb_ciphertext.decode_from_blk(_blk);
 		if (check_hash(_blk, _hash)) {
-log("sb idx ", _sb_idx);
+log("read sb pba ", _sb_idx, " hash ", _hash);
 log("sb encrypted key ", _sb_ciphertext.current_key.value);
 			_gen = _sb_ciphertext.snapshots.items[_sb_ciphertext.snapshots.newest_snap_idx()].gen;
 			_sb.copy_all_but_key_values_from(_sb_ciphertext);
