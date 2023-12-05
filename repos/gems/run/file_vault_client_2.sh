54553872d60a95e71e1c82a9f7c159676eb7b3bd
Martin Stein <martin.stein@genode-labs.com>
2023-12-05 09:11:18 +0100

    gems: add autopilot test 'file_vault_client' (write tests also when template is provided)

--- repos/gems/run/file_vault_client_2.sh
+++ repos/gems/run/file_vault_client_2.sh
@@ -3,4 +3,13 @@ ls -la file_vault/
 ls -la file_vault/dir_1
 cat file_vault/file_1
 cat file_vault/dir_1/file_2
+echo "Ein bisschen angefügter Inhalt. %&$§(" >> file_vault/file_1
+echo "Ein ganz neuer Inhalt." > file_vault/dir_1/file_2
+mkdir file_vault/dir_2
+echo "Und sogar eine neue Datei." >> file_vault/dir_2/file_3
+echo "In einem neuen Ordner." >> file_vault/dir_2/file_3
+ls -la file_vault/dir_2
+cat file_vault/file_1
+cat file_vault/dir_1/file_2
+cat file_vault/dir_2/file_3
 exit 0
