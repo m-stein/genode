d5e193144d45ac482568f7f52df4aed8f765cf70
Martin Stein <martin.stein@genode-labs.com>
2023-12-07 15:10:09 +0100

    tresor_init/tester: support vbd of size 1/1/1

--- repos/gems/src/app/tresor_init/main.cc
+++ repos/gems/src/app/tresor_init/main.cc
@@ -100,7 +100,7 @@ class Tresor_init::Main : private Vfs::Env::User, private Tresor::Module_composi
 			case INIT:
 
 				generate_req<Sb_initializer_request>(
-					INIT_SBS_SUCCEEDED, progress, (Tree_level_index)_cfg->vbd_nr_of_lvls() - 1,
+					INIT_SBS_SUCCEEDED, progress, (Tree_level_index)_cfg->vbd_nr_of_lvls(),
 					(Tree_degree)_cfg->vbd_nr_of_children(), _cfg->vbd_nr_of_leafs(),
 					(Tree_level_index)_cfg->ft_nr_of_lvls() - 1,
 					(Tree_degree)_cfg->ft_nr_of_children(), _cfg->ft_nr_of_leafs(),
--- repos/gems/src/app/tresor_tester/main.cc
+++ repos/gems/src/app/tresor_tester/main.cc
@@ -794,7 +794,7 @@ void Tresor_tester::Command::execute(bool &progress)
 		_main.reset_snap_refs();
 		Tresor_init::Configuration const &cfg { initialize() };
 		generate_req<Sb_initializer_request>(COMPLETED, progress,
-			(Tree_level_index)(cfg.vbd_nr_of_lvls() - 1),
+			(Tree_level_index)(cfg.vbd_nr_of_lvls()),
 			(Tree_degree)cfg.vbd_nr_of_children(), cfg.vbd_nr_of_leafs(),
 			(Tree_level_index)cfg.ft_nr_of_lvls() - 1,
 			(Tree_degree)cfg.ft_nr_of_children(), cfg.ft_nr_of_leafs(),
