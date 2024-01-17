00ea024f45b142e87430df7b968d3f5c674ef1ff
Martin Stein <martin.stein@genode-labs.com>
2024-01-17 11:28:40 +0100

    mmio: check offsets against size (drivers/gpu)

--- repos/os/src/drivers/gpu/intel/mmio.h
+++ repos/os/src/drivers/gpu/intel/mmio.h
@@ -23,10 +23,14 @@
 #include <types.h>
 
 
-namespace Igd { class Mmio; }
+namespace Igd {
+
+	enum { MMIO_SIZE = 0x190074 };
+	class Mmio;
+}
 
 
-class Igd::Mmio : public Platform::Device::Mmio
+class Igd::Mmio : public Platform::Device::Mmio<MMIO_SIZE>
 {
 	public:
 
@@ -1137,7 +1141,7 @@ class Igd::Mmio : public Platform::Device::Mmio
 
 	private:
 
-		struct Timer_delayer : Genode::Mmio::Delayer
+		struct Timer_delayer : Genode::Mmio<MMIO_SIZE>::Delayer
 		{
 			Timer::Connection _timer;
 			Timer_delayer(Genode::Env & env) : _timer(env) { }
@@ -1666,7 +1670,7 @@ class Igd::Mmio : public Platform::Device::Mmio
 	public:
 
 		Mmio(Platform::Device & device, Genode::Env & env)
-		: Platform::Device::Mmio(device, {0}), _delayer(env) { }
+		: Platform::Device::Mmio<MMIO_SIZE>(device, {0}), _delayer(env) { }
 
 		Delayer &delayer() { return _delayer; }
 
