e3f61931b8992657c8c0d7d02f177f6862641044
Martin Stein <martin.stein@genode-labs.com>
2024-01-31 20:48:18 +0100

    tmp

--- repos/os/src/server/vmm/ram.h
+++ repos/os/src/server/vmm/ram.h
@@ -21,29 +21,52 @@ class Ram {
 
 	private:
 
-		Genode::addr_t const _base;
-		Genode::size_t const _size;
-		Genode::addr_t const _local;
+		using Byte_range_ptr = Genode::Byte_range_ptr;
+		using addr_t = Genode::addr_t;
+		using size_t = Genode::size_t;
+
+		addr_t const _guest_base;
+		Byte_range_ptr const _local_range;
 
 	public:
+
+		Ram(addr_t guest_base, Byte_range_ptr const &local_range)
+		: _guest_base(guest_base), _local_range(local_range.start, local_range.num_bytes) { }
+
+		size_t size() const { return _local_range.num_bytes;  }
+		addr_t guest_base() const { return _guest_base;  }
+		addr_t local_base() const { return (addr_t)_local_range.start; }
+
+		Byte_range_ptr to_local_range(Byte_range_ptr const &guest_range)
+		{
+			addr_t guest_base = (addr_t)guest_range.start;
+			if (guest_base <  _guest_base ||
+			    guest_base >= _guest_base + _local_range.num_bytes ||
+			    guest_range.num_bytes == 0 ||
+			    guest_base + guest_range.num_bytes >= _guest_base + _local_range.num_bytes)
+				throw Vmm::Exception("Invalid guest physical address: ",
+				                     Genode::Hex(guest_base), " size: ", Genode::Hex(guest_range.num_bytes));
+
+			Genode::off_t offset = guest_base - _guest_base;
+			return {_local_range.start + offset, _local_range.num_bytes - offset};
+		}
 
 		Ram(Genode::addr_t const addr,
 		    Genode::size_t const sz,
 		    Genode::addr_t const local)
-		: _base(addr), _size(sz), _local(local) { }
+		: _guest_base(addr), _local_range((char *)local, sz) { }
 
-		Genode::addr_t base()  const { return _base;  }
-		Genode::size_t size()  const { return _size;  }
-		Genode::addr_t local() const { return _local; }
+		Genode::addr_t base()  const { return _guest_base;  }
+		Genode::addr_t local() const { return (addr_t)_local_range.start; }
 
-		Genode::addr_t local_address(Genode::addr_t guest, Genode::size_t size)
+		Genode::addr_t local_address(Genode::addr_t guest, Genode::size_t size1)
 		{
-			if (guest < _base || guest >= _base + _size ||
-			    size == 0 || guest + size >= _base + _size)
+			if (guest < _guest_base || guest >= _guest_base + size() ||
+			    size1 == 0 || guest + size1 >= _guest_base + size())
 				throw Vmm::Exception("Invalid guest physical address: ",
-				                     Genode::Hex(guest), " size: ", Genode::Hex(size));
+				                     Genode::Hex(guest), " size: ", Genode::Hex(size1));
 
-			return _local + (guest - _base);
+			return (addr_t)_local_range.start + (guest - _guest_base);
 		}
 };
 
