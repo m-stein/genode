116cf3b68b03d45b8ff266c7531856227b8b993f
Martin Stein <martin.stein@genode-labs.com>
2024-03-12 04:20:43 +0100

    Revert "read write sync without schedule (tmp)"
    This reverts commit 68626dad4c6c632e1a78d173692f8e7aec07bd31.

--- repos/gems/src/lib/vfs/tresor/vfs.cc
+++ repos/gems/src/lib/vfs/tresor/vfs.cc
@@ -540,7 +540,7 @@ class Vfs_tresor::Tresor_adapter : Client_data_interface, Crypto_key_files_inter
 {
 	private:
 
-		enum State { INIT, INIT_SB_CONTROL, READ, WRITE, SYNC };
+		enum State { INIT, READ, WRITE, SYNC };
 
 		struct Crypto_key
 		{
@@ -645,16 +645,6 @@ class Vfs_tresor::Tresor_adapter : Client_data_interface, Crypto_key_files_inter
 			}
 		}
 
-		bool _try_complete_init_sb_control()
-		{
-			if (_init_sb_control_ptr->complete()) {
-				ASSERT(_init_sb_control_ptr->success());
-				destroy(_vfs_env.alloc(), _init_sb_control_ptr);
-				_init_sb_control_ptr = nullptr;
-				_state = INIT;
-			}
-		}
-
 		bool _execute_tresor()
 		{
 			bool progress = false;
@@ -670,10 +660,6 @@ class Vfs_tresor::Tresor_adapter : Client_data_interface, Crypto_key_files_inter
 			case SYNC:
 				progress |= _sb_control.execute(*_sync, _block_io, _trust_anchor);
 				_try_complete_sync();
-				break;
-			case INIT_SB_CONTROL:
-				progress |= _sb_control.execute(*_init_sb_control_ptr, _block_io, _crypto, _trust_anchor)
-				_try_complete_init_sb_control();
 				break;
 			case INIT: break;
 			}
@@ -710,10 +696,21 @@ class Vfs_tresor::Tresor_adapter : Client_data_interface, Crypto_key_files_inter
 			return progress;
 		}
 
-		bool _initialized()
+		bool _try_complete_init_sb_control()
 		{
-			execute();
-			return _state != INIT_SB_CONTROL;
+			if (!_init_sb_control_ptr)
+				return true;
+
+			while (_sb_control.execute(*_init_sb_control_ptr, _block_io, _crypto, _trust_anchor)) ;
+			if (_init_sb_control_ptr->complete()) {
+
+				ASSERT(_init_sb_control_ptr->success());
+				destroy(_vfs_env.alloc(), _init_sb_control_ptr);
+				_init_sb_control_ptr = nullptr;
+				return true;
+			}
+			_wakeup_back_end_services();
+			return false;
 		}
 
 		bool _exceeds_data_file_range(addr_t start, size_t num_bytes) const
@@ -819,7 +816,7 @@ class Vfs_tresor::Tresor_adapter : Client_data_interface, Crypto_key_files_inter
 				execute();
 				return _data_operation_result;
 
-			default: break;
+			default: ASSERT_NEVER_REACHED;
 			}
 			return PENDING;
 		}
@@ -892,32 +889,28 @@ class Vfs_tresor::Tresor_adapter : Client_data_interface, Crypto_key_files_inter
 		template <typename FUNC>
 		void with_initialized_interface(FUNC && func)
 		{
-			execute();
-			if (_state != INIT_SB_CONTROL)
+			if (_try_complete_init_sb_control())
 				func(*this);
 		}
 
 		template <typename FUNC>
 		void with_rekeying(FUNC && func)
 		{
-			execute();
-			if (_state != INIT_SB_CONTROL)
+			if (_try_complete_init_sb_control())
 				func(*this, _rekeying);
 		}
 
 		template <typename FUNC>
 		void with_extending(FUNC && func)
 		{
-			execute();
-			if (_state != INIT_SB_CONTROL)
+			if (_try_complete_init_sb_control())
 				func(*this, _extending);
 		}
 
 		template <typename FUNC>
 		void with_deinitialize(FUNC && func)
 		{
-			execute();
-			if (_state != INIT_SB_CONTROL)
+			if (_try_complete_init_sb_control())
 				func(*this, _deinitialize);
 		}
 
