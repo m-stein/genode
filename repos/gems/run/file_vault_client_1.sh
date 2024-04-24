936af47416f7be6cbf2ccf71aee59bf5704c14f9
Martin Stein <martin.stein@genode-labs.com>
2024-04-24 11:58:45 +0200

    file_vault_client.run: improve bash scripts
    * structure output with headlines
    * do write tests on existing containers (container template remains unmodified)
    
    Ref #5190

--- repos/gems/run/file_vault_client_1.sh
+++ repos/gems/run/file_vault_client_1.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 echo "Hallo Welt!" > file_vault/file_1
+echo "--- file_1 (new) ---"
 cat file_vault/file_1
 echo "Ein zweiter Test." > file_vault/file_1
+echo "--- file_1 (modified) ---"
 cat file_vault/file_1
+echo "--- ls ---"
 ls -la file_vault/
 mkdir file_vault/dir_1
 echo "Eine weitere Datei." > file_vault/dir_1/file_2
 echo "Mit mehr Inhalt." >> file_vault/dir_1/file_2
+echo "--- dir_1/file_2 (new) ---"
 cat file_vault/dir_1/file_2
 echo "Und Sonderzeichen: /.:($)=%!" >> file_vault/dir_1/file_2
+echo "--- dir_1/file_2 (modified) ---"
 cat file_vault/dir_1/file_2
+echo "--- ls ---"
 ls -la file_vault/
+echo "--- ls dir_1 ---"
 ls -la file_vault/dir_1
 exit 0
--- repos/gems/run/file_vault_client_2.sh
+++ repos/gems/run/file_vault_client_2.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+echo "--- ls ---"
 ls -la file_vault/
+echo "--- ls dir_1 ---"
 ls -la file_vault/dir_1
+echo "--- file_1 ---"
 cat file_vault/file_1
+echo "--- dir_1/file_2 ---"
 cat file_vault/dir_1/file_2
+echo "Hallo Welt!" >> file_vault/file_1
+echo "Hallo Mond..." > file_vault/dir_1/file_2
+echo "... und Erde! 123" >> file_vault//dir_1/file_2
+mkdir file_vault/dir_2
+echo "Eine frisch erzeugte Datei." > file_vault/dir_2/file_3
+echo "--- file_1 (modified) ---"
+cat file_vault/file_1
+echo "--- dir_1/file_2 (modified) ---"
+cat file_vault/dir_1/file_2
+echo "--- dir_2/file_3 (new) ---"
+cat file_vault/dir_2/file_3
 exit 0
