fed82ff671f1869e417964ca9079d5a36a994d29
Martin Stein <martin.stein@genode-labs.com>
2024-05-09 07:02:39 +0200

    nic_router: remove Dhcp_allocation_tree exceptions
    This commit is part of a series that aims for removing the use of C++ exception
    as much as possible from the router as C++ exception handling can be resource
    intensive and can make code hard to understand.
    
    Ref #4729

--- repos/os/src/server/nic_router/dhcp_server.cc
+++ repos/os/src/server/nic_router/dhcp_server.cc
@@ -268,19 +268,6 @@ bool Dhcp_allocation::_higher(Mac_address const &mac) const
 }
 
 
-Dhcp_allocation &Dhcp_allocation::find_by_mac(Mac_address const &mac)
-{
-	if (mac == _mac) {
-		return *this; }
-
-	Dhcp_allocation *const allocation = child(_higher(mac));
-	if (!allocation) {
-		throw Dhcp_allocation_tree::No_match(); }
-
-	return allocation->find_by_mac(mac);
-}
-
-
 void Dhcp_allocation::print(Output &output) const
 {
 	Genode::print(output, "MAC ", _mac, " IP ", _ip);
@@ -290,18 +277,4 @@ void Dhcp_allocation::print(Output &output) const
 void Dhcp_allocation::_handle_timeout(Duration)
 {
 	_interface.dhcp_allocation_expired(*this);
-}
-
-
-/**************************
- ** Dhcp_allocation_tree **
- **************************/
-
-Dhcp_allocation &
-Dhcp_allocation_tree::find_by_mac(Mac_address const &mac) const
-{
-	if (!_tree.first()) {
-		throw No_match(); }
-
-	return _tree.first()->find_by_mac(mac);
 }
