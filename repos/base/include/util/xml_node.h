515ad75d527e40241454599c717baacc5a494849
Martin Stein <martin.stein@genode-labs.com>
2024-05-11 06:31:16 +0200

    xml_node: support attribute access via lambda
    Similar to how XML nodes can be accessed via a lambda, this commit implements
    accessing attributes of XML nodes via a lambda. There are two new methods in
    Xml_node: 'with_optional_attribute' calls a functor only if the attribute was
    found and 'with_attribute' additionally calls a functor if the attribute could
    not be found. Both methods are const and call the "found" functor with one
    argument 'Xml_attribute const &'.
    
    Ref #4729

--- repos/base/include/util/xml_node.h
+++ repos/base/include/util/xml_node.h
@@ -935,28 +935,47 @@ class Genode::Xml_node
 		T attribute_value(char const *type, T const default_value) const
 		{
 			T result = default_value;
-
-			if (!_tags.start.has_attribute())
-				return result;
-
-			for (Xml_attribute attr = _tags.start.attribute(); ; ) {
-
-				/* match */
-				if (attr.has_type(type)) {
-					attr.value(result);
-					return result;
-				}
-
-				/* end of attribute */
-				Token const next = attr._next_token();
-				if (!Xml_attribute::_valid(next))
-					break;
-
-				/* keep searching */
-				attr = Xml_attribute(next);
-			}
-
+			with_optional_attribute(type, [&] (Xml_attribute const &attr) {
+				attr.value(result); });
 			return result;
+		}
+
+		/**
+		 * Apply functor 'found_fn' to attribute of specified type
+		 *
+		 * The functor is called with the attribute as argument. If no matching
+		 * attribute exists, the functor 'not_found_fn' is called instead.
+		 */
+		void with_attribute(char const *type, auto const &found_fn, auto const &not_found_fn) const
+		{
+			if (_tags.start.has_attribute())
+				for (Xml_attribute attr = _tags.start.attribute(); ; ) {
+
+					/* match */
+					if (attr.has_type(type)) {
+						found_fn(attr);
+						return;
+					}
+					/* end of attribute */
+					Token const next = attr._next_token();
+					if (!Xml_attribute::_valid(next))
+						break;
+
+					/* keep searching */
+					attr = Xml_attribute(next);
+				}
+			not_found_fn();
+		}
+
+		/**
+		 * Apply functor 'fn' to attribute of specified type
+		 *
+		 * The functor is called with the attribute as argument. If no matching
+		 * attribute exists, the functor is not called.
+		 */
+		void with_optional_attribute(char const *type, auto const &fn) const
+		{
+			with_attribute(type, [&] (Attribute const &attr) { fn(attr); }, []{});
 		}
 
 		/**
