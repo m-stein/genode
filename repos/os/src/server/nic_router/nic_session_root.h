93979d13698605bf05bb11cc3ff86a1658e8898e
Martin Stein <martin.stein@genode-labs.com>
2024-05-13 09:30:09 +0200

    nic_router: remove Report::Empty exception
    This commit is part of a series that aims for removing the use of C++ exception
    as much as possible from the router as C++ exception handling can be resource
    intensive and can make code hard to understand.
    
    Ref #4729

--- repos/os/src/server/nic_router/nic_session_root.h
+++ repos/os/src/server/nic_router/nic_session_root.h
@@ -117,6 +117,7 @@ class Net::Nic_session_component : private Nic_session_component_base,
 				Domain_name determine_domain_name() const override;
 				void handle_config(Configuration const &config) override { _config = config; }
 				Genode::Session_label const &label() const override { return _label; }
+				bool report_empty() const override { return _session_env.report_empty(); };
 				void report(Genode::Xml_generator &xml) const override { _session_env.report(xml); };
 				void handle_domain_ready_state(bool state) override;
 				bool interface_link_state() const override;
