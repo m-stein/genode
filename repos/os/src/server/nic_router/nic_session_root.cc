e0dce48e3b02d8404b50ebb6a270626f533ceab7
Martin Stein <martin.stein@genode-labs.com>
2024-05-16 13:09:42 +0200

    nic_router: remove Mac_allocator::Alloc_failed exception

--- repos/os/src/server/nic_router/nic_session_root.cc
+++ repos/os/src/server/nic_router/nic_session_root.cc
@@ -294,11 +294,14 @@ Net::Nic_session_root::Nic_session_root(Env               &env,
 	_env                                  { env },
 	_timer                                { timer },
 	_mac_alloc                            { MAC_ALLOC_BASE },
-	_router_mac                           { _mac_alloc.alloc() },
 	_config                               { config },
 	_shared_quota                         { shared_quota },
 	_interfaces                           { interfaces }
-{ }
+{
+	_mac_alloc.alloc().with_result(
+		[&] (Mac_address const &mac){ _router_mac.construct(mac); },
+		[] (auto) { ASSERT_NEVER_REACHED; });
+}
 
 
 Nic_session_component *Net::Nic_session_root::_create_session(char const *args)
@@ -309,25 +312,29 @@ Nic_session_component *Net::Nic_session_root::_create_session(char const *args)
 			_env, _shared_quota, args,
 			[&] (Session_env &session_env, void *session_at, Ram_dataspace_capability ram_ds)
 			{
+				Nic_session_component *result { };
 				Session_label const label { label_from_args(args) };
-				Mac_address const mac { _mac_alloc.alloc() };
-				try {
-					return construct_at<Nic_session_component>(
-						session_at, session_env,
-						Arg_string::find_arg(args, "tx_buf_size").ulong_value(0),
-						Arg_string::find_arg(args, "rx_buf_size").ulong_value(0),
-						_timer, mac, _router_mac, label, _interfaces,
-						_config(), ram_ds);
-				}
-				catch (...) {
-					_mac_alloc.free(mac);
-					throw;
-				}
+				_mac_alloc.alloc().with_result(
+					[&] (auto mac) {
+						try {
+							result = construct_at<Nic_session_component>(
+								session_at, session_env,
+								Arg_string::find_arg(args, "tx_buf_size").ulong_value(0),
+								Arg_string::find_arg(args, "rx_buf_size").ulong_value(0),
+								_timer, mac, *_router_mac, label, _interfaces,
+								_config(), ram_ds);
+						}
+						catch (...) {
+							_mac_alloc.free(mac);
+							throw;
+						}
+					},
+					[&] (auto) {
+						_invalid_downlink("failed to allocate MAC address");
+						throw Service_denied();
+					});
+				return result;
 			});
-	}
-	catch (Mac_allocator::Alloc_failed) {
-		_invalid_downlink("failed to allocate MAC address");
-		throw Service_denied();
 	}
 	catch (Region_map::Invalid_dataspace) {
 		_invalid_downlink("Failed to attach RAM");
